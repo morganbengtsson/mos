f8e7bcf5e0ae41469cfb9dcb806f1f9cc430f020
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2014-04-26 23:17:18 +0200

    Render changes.

--- mo/Renderer.cpp
+++ mo/Renderer.cpp
@@ -38,10 +38,10 @@ namespace mo {
         glDepthMask(true);
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
+        
         auto standard_vertex_shader = ogli::createShader(ogli::loadText("standard.vs"), GL_VERTEX_SHADER);
         auto standard_fragment_shader = ogli::createShader(ogli::loadText("standard.fs"), GL_FRAGMENT_SHADER);
-
+        
         standard_program_ = ogli::createProgram();
         ogli::attachShader(standard_program_, standard_vertex_shader);
         ogli::attachShader(standard_program_, standard_fragment_shader);
@@ -53,10 +53,35 @@ namespace mo {
 
         standard_mvp_uniform_ = ogli::createUniform(standard_program_, "model_view_projection");
         standard_mv_uniform_ = ogli::createUniform(standard_program_, "model_view");
-        standard_texture_uniform_ = ogli::createUniform(standard_program_, "texture");       
+        standard_texture_uniform_ = ogli::createUniform(standard_program_, "texture");
+        standard_camera_position_ = ogli::createUniform(standard_program_, "camera_position");
+        
+        addProgram("standard");
+        
     }
 
     Renderer::~Renderer() {
+    }
+    
+    void Renderer::addProgram(const std::string name){
+        auto vertex_shader = ogli::createShader(ogli::loadText(name + ".vs"), GL_VERTEX_SHADER);
+        auto fragment_shader = ogli::createShader(ogli::loadText(name + ".fs"), GL_FRAGMENT_SHADER);
+               
+        auto program = ogli::createProgram();
+        ogli::attachShader(program, vertex_shader);
+        ogli::attachShader(program, fragment_shader);
+        ogli::bindAttribute(program, position_attribute_3P3N2UV_);
+        ogli::bindAttribute(program, normal_attribute_3P3N2UV_);
+        ogli::bindAttribute(program, uv_attribute_3P3N2UV_);
+
+        ogli::linkProgram(program);
+
+        auto mvp_uniform = ogli::createUniform(standard_program_, "model_view_projection");
+        auto mv_uniform = ogli::createUniform(standard_program_, "model_view");
+        auto texture_uniform = ogli::createUniform(standard_program_, "texture");
+        auto camera_position_uniform = ogli::createUniform(standard_program_, "camera_position");        
+        
+        programs_.insert(ProgramPair(name, ProgramData{program, mvp_uniform, mv_uniform, texture_uniform, camera_position_uniform}));
     }
 
     void Renderer::clear(const glm::vec3 color) {
@@ -64,7 +89,7 @@ namespace mo {
         ogli::clearColor(glm::vec4(color.r, color.g, color.b, 0.0f));
     }
     
-    void Renderer::render(const Model & model, const glm::mat4 transform, const glm::mat4 view, const glm::mat4 projection) {
+    void Renderer::render(const Model & model, const glm::mat4 transform, const glm::mat4 view, const glm::mat4 projection, const std::string program_name) {
     if (array_buffers_.find(model.mesh()->id()) == array_buffers_.end()) {
         array_buffers_.insert(ArrayPair(model.mesh()->id(),
                 ogli::createArrayBuffer(model.mesh()->verticesBegin(), model.mesh()->verticesEnd())));
@@ -81,17 +106,32 @@ namespace mo {
 
     glm::mat4 mv = projection * view;
     glm::mat4 mvp = projection * view * transform * model.transform();
-    ogli::useProgram(standard_program_);
+    
+    //ogli::useProgram(standard_program_);
+    
+    ogli::useProgram(programs_.at(program_name).program);
+    
+    //ogli::useProgram(std::get<0>(programs_.at(program_name)));
     ogli::bindBuffer(array_buffers_.at(model.mesh()->id()));
     ogli::bindBuffer(element_array_buffers_.at(model.mesh()->id()));
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, textures_.at(model.texture()->id()));
+    
+    //TODO: General lighting, this is a hack.
+    glm::mat3 rot_mat(view);
+    glm::vec3 dir(view[3]);
 
-    ogli::uniform(standard_texture_uniform_);
-    ogli::uniform(standard_mv_uniform_, mv);
-    ogli::uniform(standard_mvp_uniform_, mvp);
-
+    glm::vec3 camera_pos = -dir * rot_mat;    
+  
+    std::cout << camera_pos.x << " " << camera_pos.y << " " << camera_pos.z << std::endl;
+    
+    ogli::uniform(programs_.at(program_name).mvp, mvp);
+    ogli::uniform(programs_.at(program_name).mv, mv);    
+    ogli::uniform(programs_.at(program_name).texture);
+    ogli::uniform(programs_.at(program_name).camera_position, camera_pos);   
+    
+    
     ogli::attribute(position_attribute_3P3N2UV_);
     ogli::attribute(normal_attribute_3P3N2UV_);
     ogli::attribute(uv_attribute_3P3N2UV_);    
--- mo/Renderer.h
+++ mo/Renderer.h
@@ -12,22 +12,33 @@
 #include <glm/glm.hpp>
 #include <iostream>
 #include <map>
+#include <tuple>
 
 #include "Vertex.h"
 #include "Texture2D.h"
 #include "Model.h"
 
-namespace mo {
 
+namespace mo {
+    struct ProgramData{
+        ogli::Program program;
+        ogli::Uniform mvp;
+        ogli::Uniform mv;
+        ogli::Uniform texture;
+        ogli::Uniform camera_position;
+    };
+    
     class Renderer {
     public:
 
         typedef std::pair<unsigned int, ogli::ArrayBuffer> ArrayPair;
         typedef std::pair<unsigned int, ogli::ElementArrayBuffer> ElementPair;
         typedef std::pair<unsigned int, ogli::TextureBuffer> TexturePair;
-
+        typedef std::pair<std::string, ProgramData> ProgramPair;
+        
         Renderer();
-        void render(const Model & model, const glm::mat4 transform, const glm::mat4 view, const glm::mat4 projection);
+        void addProgram(const std::string name);
+        void render(const Model & model, const glm::mat4 transform, const glm::mat4 view, const glm::mat4 projection, const std::string program_name = "standard");
         void clear(const glm::vec3 color);
 
         virtual ~Renderer();
@@ -40,7 +51,9 @@ namespace mo {
         ogli::Uniform standard_mvp_uniform_;
         ogli::Uniform standard_mv_uniform_;
         ogli::Uniform standard_texture_uniform_;
-
+        ogli::Uniform standard_camera_position_;
+        
+        std::map<std::string, ProgramData> programs_;
         std::map<unsigned int, ogli::TextureBuffer> textures_;
         std::map<unsigned int, ogli::ArrayBuffer> array_buffers_;
         std::map<unsigned int, ogli::ElementArrayBuffer> element_array_buffers_;
