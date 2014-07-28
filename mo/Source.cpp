b6450b12dff08c552afeeb91cd29a6e41cc36738
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2014-07-28 19:53:00 +0200

    Missing files again.

---
+++ mo/Source.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   Source.cpp
+ * Author: morgan
+ * 
+ * Created on July 28, 2014, 3:47 PM
+ */
+
+#include "Source.h"
+
+namespace mo {
+
+    Source::~Source() {
+    }
+    
+    const Source::Sounds::const_iterator Source::begin() const{
+        return sounds_.begin();
+    }
+    const Source::Sounds::const_iterator Source::end() const{
+        return sounds_.end();
+    }
+
+    unsigned int Source::id() const{
+        return id_;
+    }
+}
