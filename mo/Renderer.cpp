21365ac50bda697591c21c097319a72080394516
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2014-12-14 14:40:15 +0100

    Correct rendering.

--- mo/Renderer.cpp
+++ mo/Renderer.cpp
@@ -109,18 +109,16 @@ namespace mo {
                 "float a = 1.0;\n"
                 "float b = 1.0;\n"
                 "float att = 1.0 / (1.0 + a*dist + b*dist*dist);\n"
-                "vec4 diffuse = vec4(indirect.w * att * diffuse_contribution* light_diffuse_color, 1.0) * diffuse_color;\n"
-                
+                "vec4 diffuse = vec4(att * diffuse_contribution* light_diffuse_color, 1.0) * diffuse_color;\n"                
                 
                 "vec3 surface_to_view = normalize(fragment_position);\n"
                 "vec3 reflection = reflect(normal, -surface_to_light);\n"
                 "float secular_contribution = pow(max(0.0, dot(surface_to_view, reflection)), material_specular_exponent);\n"
                 "vec4 specular = vec4(secular_contribution * light_specular_color * material_specular_color, 1.0);\n"
 
-                //"gl_FragColor = vec4(indirect.xyz*light_diffuse_color + diffuse.xyz, 1.0);\n"
-                "gl_FragColor = vec4(indirect.xyz + diffuse.xyz + specular.xyz, 1.0);\n"
-                //"gl_FragColor = vec4(indirect.xyz, 1.0);\n"
-                //"gl_FragColor = diffuse;\n"
+                "vec4 diffuse_indirect = indirect * diffuse_color;"
+                
+                "gl_FragColor = vec4(diffuse.xyz + diffuse_indirect.xyz + specular.xyz, 1.0);\n"               
                 
                 "}\n";
         add_vertex_program("standard", standard_vertex_source, standard_fragment_source);
