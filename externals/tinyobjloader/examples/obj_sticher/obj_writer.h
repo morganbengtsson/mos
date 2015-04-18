7db82e43790a4ebb5f9711d8d795f2d4a8f0e684
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2015-04-18 20:45:48 +0200

    Remove obj deps and cleaner build.

--- externals/tinyobjloader/examples/obj_sticher/obj_writer.h
+++
@@ -1,9 +0,0 @@
-#ifndef __OBJ_WRITER_H__
-#define __OBJ_WRITER_H__
-
-#include "../../tiny_obj_loader.h"
-
-extern bool WriteObj(const std::string& filename, const std::vector<tinyobj::shape_t>& shapes, const std::vector<tinyobj::material_t>& materials);
-
-
-#endif // __OBJ_WRITER_H__
