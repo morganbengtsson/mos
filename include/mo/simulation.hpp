16376e050ed06b72c68bc6c3f8fa1c88e709dd90
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2015-07-18 21:35:31 +0200

    Simulation.hpp

---
+++ include/mo/simulation.hpp
@@ -0,0 +1,26 @@
+#include <glm/glm.hpp>
+#include "model.hpp"
+
+namespace mo {
+
+template<class It>
+void selections(It begin, It end, const glm::vec3 & position, glm::vec3 & direction){
+    mo::Model * closest = nullptr;
+    float distance = 10000.0f;
+    for (auto it = begin; it != end; it++){
+        auto & model = *it;
+        auto res = model.box.intersect(position, glm::normalize(direction), 0.0f, 1000.0f);
+        model.selected(false);
+        if (res.intersects){
+            if (res.distance < distance){
+                distance = res.distance;
+                closest = &model;
+            }
+        }
+    }
+    if (closest != nullptr){
+        closest->selected(true);
+    }
+}
+
+}
