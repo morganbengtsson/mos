bd37bde0a904e8f107bf3b8e136df4ea9a9f48c9
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2015-07-23 19:54:00 +0200

    Quad model.

---
+++ include/mos/quad.hpp
@@ -0,0 +1,22 @@
+#ifndef MOS_QUAD_HPP
+#define MOS_QUAD_HPP
+
+#include "model.hpp"
+#include "texture2d.hpp"
+#include "mesh.hpp"
+
+#include <memory>
+
+namespace mos {
+
+class Quad : public mos::Model
+{
+public:
+    Quad(const std::shared_ptr<Mesh> mesh,
+         const std::shared_ptr<Texture2D> texture);
+    virtual ~Quad();
+};
+
+}
+
+#endif // MOS_QUAD_HPP
