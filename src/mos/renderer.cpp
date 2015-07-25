8326e059149a2e941bc73d0cbd93fd97e4667a97
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2015-07-25 15:30:42 +0200

    Checkpoint.

--- src/mos/renderer.cpp
+++ src/mos/renderer.cpp
@@ -58,8 +58,8 @@ Renderer::Renderer() :
     std::string text_frag_source = text("assets/shaders/text_330.frag");
     add_vertex_program("text", text_vert_source, text_frag_source);
 
-    add_vertex_program("effect", text("assets/shaders/effect.vert"),
-                       text("assets/shaders/effect.frag"));
+    add_vertex_program("effect", text("assets/shaders/effect_330.vert"),
+                       text("assets/shaders/effect_330.frag"));
 
     std::string particles_vert_source = text("assets/shaders/particles.vert");
     std::string particles_frag_source = text("assets/shaders/particles.frag");
