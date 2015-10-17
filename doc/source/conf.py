16ba0ba779f2ec3ee71224341e6d081c2a36e222
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2015-10-17 13:58:24 +0200

    Uml stuff.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,7 +13,7 @@ if read_the_docs_build:
 
 # -- General configuration ------------------------------------------------
 
-extensions = ["breathe"]
+extensions = ["breathe","sphinx_scruffy"]
 breathe_projects = {"MOS": "../build/xml/"}
 breate_default_project = "MOS"
 
--- include/mos/audio/player.hpp
+++ include/mos/audio/player.hpp
@@ -119,7 +119,8 @@ namespace mos {
 		* @brief listener_orientation
 		* @param orientation, up
 		*/
-        void listener_orientation(const glm::vec3 orientation, const glm::vec3 up = glm::vec3(0.0f, 0.0f, 1.0f));
+        void listener_orientation(const glm::vec3 orientation,
+                                  const glm::vec3 up = glm::vec3(0.0f, 0.0f, 1.0f));
         
 		/**
 		* Get the listener velocity.
