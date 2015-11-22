ada41ef4ae084fc904311f8db90976aeb643843e
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2015-11-22 20:09:12 +0100

    Check.

--- src/mos/experimental/button.cpp
+++ src/mos/experimental/button.cpp
@@ -11,9 +11,9 @@ Button::Button(const Text & text) :text_(text) {
     mos::Mesh mesh({v1, v2, v3, v4}, {0, 2, 1, 0, 3, 2});
     rectangle_.mesh = std::make_shared<Mesh>(mesh);
     rectangle_.transform(glm::scale(glm::mat4(1.0f), glm::vec3(3.0f, 3.0f, 3.0f)));
-    mos::Material material(glm::vec3(1.0f), glm::vec3(1.0f));
+    mos::Material material(glm::vec3(1.0f), glm::vec3(0.5f, 0.5f, 1.0f));
     material.opacity = 0.5f;
-    rectangle_.opacity = 1.0f;
+    rectangle_.opacity = 0.1f;
     rectangle_.material = std::make_shared<Material>(material);
     rectangle_.receives_light = false;
     rectangle_.shader = Model::Shader::STANDARD;
@@ -24,7 +24,10 @@ Button::~Button() {
 }
 
 Model Button::model() {
-    return rectangle_;
+    mos::Model out;
+    out.models.push_back(rectangle_);
+    out.models.push_back(text_.model());
+    return out;
 }
 
 }
