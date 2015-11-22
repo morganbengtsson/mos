e84209a37173f23fc83bb80bbeca7a7929c9b917
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2015-11-22 15:53:18 +0100

    Working.

--- src/mos/experimental/button.cpp
+++ src/mos/experimental/button.cpp
@@ -10,14 +10,14 @@ Button::Button(const Text & text) :text_(text) {
 
     mos::Mesh mesh({v1, v2, v3, v4}, {0, 2, 1, 0, 3, 2});
     rectangle_.mesh = std::make_shared<Mesh>(mesh);
-    rectangle_.mesh->invalidate();
-    rectangle_.transform(glm::scale(glm::mat4(1.0f), glm::vec3(10.0f, 10.0f, 10.0f)));
+    rectangle_.transform(glm::scale(glm::mat4(1.0f), glm::vec3(3.0f, 3.0f, 3.0f)));
     mos::Material material(glm::vec3(1.0f), glm::vec3(1.0f));
     material.opacity = 0.5f;
     rectangle_.opacity = 1.0f;
     rectangle_.material = std::make_shared<Material>(material);
     rectangle_.receives_light = false;
-    rectangle_.shader = Model::Shader::TEXT;
+    rectangle_.shader = Model::Shader::STANDARD;
+     rectangle_.mesh->invalidate();
 }
 
 Button::~Button() {
