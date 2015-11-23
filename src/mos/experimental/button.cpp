3d1086e81f0f9b67bdaf1ee5106cb51a77ea1056
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2015-11-23 16:01:37 +0100

    Text done.

--- src/mos/experimental/button.cpp
+++ src/mos/experimental/button.cpp
@@ -13,8 +13,8 @@ Button::Button(const Text & text) :text_(text) {
     rectangle_.mesh = std::make_shared<Mesh>(mesh);
     rectangle_.transform(glm::scale(glm::mat4(1.0f), glm::vec3(text_.width()*2.0f + padding * 2.0f, text_.height() + padding * 2.0f, 1.0f)));
     rectangle_.transform(glm::translate(rectangle_.transform(), glm::vec3(0.0f, 0.0f, -0.0f)));
-    mos::Material material(glm::vec3(1.0f), glm::vec3(0.8f, 0.8f, 1.0f));
-    material.opacity = 0.5f;
+    mos::Material material(glm::vec3(1.0f), glm::vec3(0.01f));
+    material.opacity = 0.8f;
     rectangle_.material = std::make_shared<Material>(material);
     rectangle_.receives_light = false;
     rectangle_.shader = Model::Shader::STANDARD;
