c9f51745fa18af28c7e2d4011aea5d04bb3f3865
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2015-11-23 20:17:33 +0100

    Menu file.

---
+++ include/mos/experimental/menu.hpp
@@ -0,0 +1,22 @@
+#ifndef MENU_HPP
+#define MENU_HPP
+
+#include "button.hpp"
+#include <initializer_list>
+#include <vector>
+
+namespace   mos {
+class Menu
+{
+public:
+    using Buttons = std::vector<Button>;
+    Menu();
+    Menu(const std::initializer_list<Button> & buttons);
+    void add(const Button & button);
+    mos::Model model();
+    ~Menu();
+private:
+    Buttons buttons_;
+};
+}
+#endif // MENU_HPP
---
+++ src/mos/experimental/menu.cpp
@@ -0,0 +1,31 @@
+#include <experimental/menu.hpp>
+
+namespace mos {
+
+Menu::Menu() {
+
+}
+
+Menu::Menu(const std::initializer_list<Button> &buttons) : buttons_(buttons.begin(), buttons.end()){
+
+}
+
+Menu::~Menu() {
+}
+
+void Menu::add(const Button &button) {
+    buttons_.push_back(button);
+}
+
+Model Menu::model(){
+    mos::Model out;
+    for (int i = 0; i < buttons_.size(); i++) {
+        auto model = buttons_[i].model();
+        model.transform(glm::translate(model.transform(), glm::vec3(0.0f, -i * 14.0f, 0.0f)));
+        out.models.push_back(model);
+    }
+    return out;
+}
+
+
+}
