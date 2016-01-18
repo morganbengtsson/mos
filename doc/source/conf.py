46407425a5faeaf7ebaf811d118a608a10453b7c
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-01-18 17:42:20 +0100

    Rename rst file.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'home'
 
 # General information about the project.
 project = 'MOS'
