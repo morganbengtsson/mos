005690e3fe42825bc54c9d29a736904105da70c5
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-01-30 21:08:35 +0100

    Read the docs fix.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'home'
+master_doc = 'index'
 
 # General information about the project.
 project = 'MOS'
