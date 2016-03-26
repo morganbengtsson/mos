05d38af363d728314e9f656c0154b1e5038609c0
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-03-26 12:45:39 +0100

    Check

--- src/mos/graphics/text.cpp
+++ src/mos/graphics/text.cpp
@@ -19,44 +19,46 @@ Text::~Text() {}
 std::string Text::text() const { return text_; }
 
 void Text::text(const std::string &text) {
+  if (text_.compare(text) != 0) {
+    text_ = text;
+    float index = 0.0f;
+    model_.mesh->clear();
+    for (const char &c : text_) {
+      auto character = font_.characters.at(c);
+      float u1 = character.rect_x / ((float)model_.texture->width());
+      float u2 = (character.rect_x + character.rect_w) /
+                 (float)model_.texture->width();
+      float v1 = character.rect_y / ((float)model_.texture->height());
+      float v2 = ((character.rect_y + character.rect_h) /
+                  ((float)model_.texture->height()));
 
-  text_ = text;
-  float index = 0.0f;
-  model_.mesh->clear();
-  for (const char &c : text_) {
-    auto character = font_.characters.at(c);
-    float u1 = character.rect_x / ((float)model_.texture->width());
-    float u2 =
-        (character.rect_x + character.rect_w) / (float)model_.texture->width();
-    float v1 = character.rect_y / ((float)model_.texture->height());
-    float v2 = ((character.rect_y + character.rect_h) /
-                ((float)model_.texture->height()));
+      // float offset_y = ((float) model_.texture->height()) -
+      // character.offset_y;
+      float offset_y = character.offset_y - font_.ascender();
+      float offset_x = character.offset_x;
+      float rect_h = -character.rect_h;
 
-    // float offset_y = ((float) model_.texture->height()) - character.offset_y;
-    float offset_y = character.offset_y - font_.ascender();
-    float offset_x = character.offset_x;
-    float rect_h = -character.rect_h;
-
-    model_.mesh->add(
-        Vertex(glm::vec3(index + offset_x, rect_h + offset_y, 0.0f),
-               glm::vec3(0.0f), glm::vec2(u1, v2)));
-    model_.mesh->add(
-        Vertex(glm::vec3(index + character.rect_w + offset_x, offset_y, 0.0f),
-               glm::vec3(0.0f), glm::vec2(u2, v1)));
-    model_.mesh->add(Vertex(glm::vec3(index + offset_x, offset_y, 0.0f),
-                            glm::vec3(0.0f), glm::vec2(u1, v1)));
-    model_.mesh->add(
-        Vertex(glm::vec3(index + offset_x, rect_h + offset_y, 0.0f),
-               glm::vec3(0.0f), glm::vec2(u1, v2)));
-    model_.mesh->add(Vertex(
-        glm::vec3(index + character.rect_w + offset_x, rect_h + offset_y, 0.0f),
-        glm::vec3(0.0f), glm::vec2(u2, v2)));
-    model_.mesh->add(
-        Vertex(glm::vec3(index + character.rect_w + offset_x, offset_y, 0.0f),
-               glm::vec3(0.0f), glm::vec2(u2, v1)));
-    index += character.advance + spacing;
+      model_.mesh->add(
+          Vertex(glm::vec3(index + offset_x, rect_h + offset_y, 0.0f),
+                 glm::vec3(0.0f), glm::vec2(u1, v2)));
+      model_.mesh->add(
+          Vertex(glm::vec3(index + character.rect_w + offset_x, offset_y, 0.0f),
+                 glm::vec3(0.0f), glm::vec2(u2, v1)));
+      model_.mesh->add(Vertex(glm::vec3(index + offset_x, offset_y, 0.0f),
+                              glm::vec3(0.0f), glm::vec2(u1, v1)));
+      model_.mesh->add(
+          Vertex(glm::vec3(index + offset_x, rect_h + offset_y, 0.0f),
+                 glm::vec3(0.0f), glm::vec2(u1, v2)));
+      model_.mesh->add(Vertex(glm::vec3(index + character.rect_w + offset_x,
+                                        rect_h + offset_y, 0.0f),
+                              glm::vec3(0.0f), glm::vec2(u2, v2)));
+      model_.mesh->add(
+          Vertex(glm::vec3(index + character.rect_w + offset_x, offset_y, 0.0f),
+                 glm::vec3(0.0f), glm::vec2(u2, v1)));
+      index += character.advance + spacing;
+    }
+    model_.mesh->invalidate();
   }
-  model_.mesh->invalidate();
 }
 
 float Text::width() const {
@@ -68,13 +70,15 @@ float Text::width() const {
 float Text::height() const { return font_.height(); }
 
 void Text::position(const glm::vec2 &position) {
-  model_.transform = glm::translate(glm::mat4(1.0f), glm::vec3(position.x, position.y, 0.0f));
+  model_.transform =
+      glm::translate(glm::mat4(1.0f), glm::vec3(position.x, position.y, 0.0f));
 }
 
 glm::vec2 Text::position() { return glm::vec2(model_.position()); }
 
 void Text::scale(const float scale) {
-  model_.transform = glm::scale(model_.transform, glm::vec3(scale, scale, scale));
+  model_.transform =
+      glm::scale(model_.transform, glm::vec3(scale, scale, scale));
 }
 
 void Text::material(const std::shared_ptr<Material> &material) {
