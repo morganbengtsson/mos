b31ac088f7c51c65fc89bd047e771851eb23f412
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-04-24 17:25:19 +0200

    FogExp rename.

--- src/mos/graphics/batch.cpp
+++ src/mos/graphics/batch.cpp
@@ -4,7 +4,7 @@ Batch::Batch() {}
 
 Batch::Batch(const std::initializer_list<Model> &models, const glm::mat4 &view,
              const glm::mat4 &projection, const glm::vec2 &resolution,
-             const Light &light, const Fog &fog, const Shader &shader,
+             const Light &light, const FogExp &fog, const Shader &shader,
              const Draw &draw)
     : Batch(models.begin(), models.end(), view, projection, resolution, light,
             fog, shader, draw) {}
