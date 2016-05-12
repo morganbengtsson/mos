d543b016a9351d917c47f658deca05986d51a60c
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-05-12 12:07:17 +0200

    Types.

--- include/mos/types.hpp
+++ include/mos/types.hpp
@@ -3,11 +3,13 @@
 
 #include <memory>
 #include <mos/audio/sound.hpp>
+#include <mos/audio/stream.hpp>
 #include <mos/graphics/mesh.hpp>
 
 namespace mos {
 using SharedSound = std::shared_ptr<Sound>;
 using SharedMesh = std::shared_ptr<Mesh>;
+using SharedStream = std::shared_ptr<Stream>;
 }
 
 #endif // TYPES_HPP
