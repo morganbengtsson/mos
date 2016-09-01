6b5cc6ea6e9855d9425f00769eb41d2f597930c2
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-09-01 18:35:35 +0200

    Remove class.

--- src/mos/render/lightmaps.cpp
+++
@@ -1,8 +0,0 @@
-#include <mos/render/lightmaps.hpp>
-
-namespace mos{
-
-Lightmaps::Lightmaps(const Lightmaps::TexPtr &lightmap0,
-                     const Lightmaps::TexPtr &lightmap1, const float mix)
-    : first(lightmap0), second(lightmap1), mix(mix) {}
-}
