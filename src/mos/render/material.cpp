cacd93e45858cab26b02b121562ed6e5bf79dff6
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-09-22 12:03:35 +0200

    Material path constructor.

--- src/mos/render/material.cpp
+++ src/mos/render/material.cpp
@@ -9,25 +9,8 @@ namespace mos {
       specular_exponent(specular_exponent) {}
 
   Material::Material(const std::string &path) {
-
-  }
-
-  Material::~Material() {}
-
-  SharedMaterial Material::load(const std::string &path) {
-    glm::vec3 ambient;
-    glm::vec3 diffuse(1.0f, 1.0f, 0.0f);
-    glm::vec3 specular;
-    float opacity, specular_exponent;
-
-    if (path.empty()) {
-      return std::make_shared<Material>(ambient, diffuse, specular, opacity,
-                                        specular_exponent);
-    }
-
     if (path.substr(path.find_last_of(".") + 1) == "material") {
       std::ifstream is(path, std::ios::binary);
-
       is.read((char *)&ambient, sizeof(glm::vec3));
       is.read((char *)&diffuse, sizeof(glm::vec3));
       is.read((char *)&specular, sizeof(glm::vec3));
@@ -37,8 +20,14 @@ namespace mos {
       throw std::runtime_error(path.substr(path.find_last_of(".")) +
                                " file format is not supported.");
     }
-    return std::make_shared<Material>(ambient, diffuse, specular, opacity,
-                                      specular_exponent);
+  }
 
+  Material::~Material() {}
+
+  SharedMaterial Material::load(const std::string &path) {
+    if (path.empty()) {
+      return std::make_shared<Material>();
+    }
+    return std::make_shared<Material>(path);
   }
 }
