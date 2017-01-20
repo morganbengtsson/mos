de874f0531745adedae8c9ac1010ebef927da1cf
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-01-20 15:22:29 +0100

    new fog

--- include/mos/render/fog_linear.hpp
+++ include/mos/render/fog_linear.hpp
@@ -7,10 +7,16 @@ namespace mos {
 class FogLinear {
 public:
   FogLinear(const glm::vec3 &color = glm::vec3(1.0f),
-            const float near = 2000.0f, const float far = 3000.0f);
+            const float near = 2000.0f, const float far = 3000.0f,
+            const float linear_attenuation_factor = 1.0f,
+            const float exponential_attenuation_factor = 0.0f,
+            const float exponential_attenuation_squared_factor = 0.0f);
   glm::vec3 color;
   float near;
   float far;
+  float linear_attenuation_factor;
+  float exponential_attenuation_factor;
+  float exponential_attenuation_squared_factor;
   ~FogLinear();
 };
 }
--- src/mos/render/fog_linear.cpp
+++ src/mos/render/fog_linear.cpp
@@ -1,9 +1,15 @@
 #include <mos/render/fog_linear.hpp>
 
 namespace mos {
-FogLinear::FogLinear(const glm::vec3 &color, const float near, const float far)
-    : color(color), near(near), far(far) {
-}
+FogLinear::FogLinear(const glm::vec3 &color,
+                     const float near,
+                     const float far,
+                     const float linear_attenuation_factor,
+                     const float exponential_attenuation_factor,
+                     const float exponential_attenuation_squared_factor)
+    : color(color), near(near), far(far), linear_attenuation_factor(linear_attenuation_factor),
+      exponential_attenuation_factor(exponential_attenuation_factor),
+      exponential_attenuation_squared_factor(exponential_attenuation_squared_factor) {}
 
 FogLinear::~FogLinear() {}
 }
