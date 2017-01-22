9216fe14648bdb3225d8a40a6e4de7a8979885d2
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-01-22 14:56:31 +0100

    One decal

--- include/mos/render/decal.hpp
+++ include/mos/render/decal.hpp
@@ -7,6 +7,10 @@
 namespace mos {
 class Decal {
 public:
+  Decal(const glm::mat4 &transform,
+        const glm::mat4 &projection,
+        const glm::mat4 &view,
+        const mos::SharedTexture &texture);
   glm::mat4 transform;
   glm::mat4 projection;
   glm::mat4 view;
--- src/mos/render/decal.cpp
+++ src/mos/render/decal.cpp
@@ -1,1 +1,8 @@
 #include <mos/render/decal.hpp>
+mos::Decal::Decal(const glm::mat4 &transform,
+                  const glm::mat4 &projection,
+                  const glm::mat4 &view,
+                  const mos::SharedTexture &texture)
+    : transform(transform), projection(projection), view(view), texture(texture) {
+  texture->wrap = mos::Texture::Wrap::CLAMP_TO_BORDER;
+}
