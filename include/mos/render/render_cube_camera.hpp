a371770e7e789a95fe2fa9d95775d2ebb3b87960
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-02-06 14:14:18 +0100

    check

--- include/mos/render/render_cube_camera.hpp
+++ include/mos/render/render_cube_camera.hpp
@@ -3,7 +3,7 @@
 
 #include <glm/glm.hpp>
 #include <array>
-
+#include <mos/render/render_camera.hpp>
 
 namespace mos {
 class RenderCubeCamera {
@@ -11,17 +11,13 @@ public:
   RenderCubeCamera(const glm::vec3 &position = glm::vec3(0.0f, 0.0f, 2.0f),
                    const glm::vec2 &resolution = glm::vec2(128.0f, 128.0f));
 
-  glm::vec3 position() const;
-  void position(const glm::vec3 &position);
-  float aspect_ratio() const;
-  std::array<glm::mat4, 6> views;
-  const glm::vec2 resolution;
-  const glm::mat4 projection;
-
+  std::array<mos::RenderCamera, 6> cameras;
 
 private:
   void update_views();
-  glm::vec3 position_;
+  const glm::mat4 projection;
+  const glm::vec3 up;
+
 };
 }
 
--- src/mos/render/render_cube_camera.cpp
+++ src/mos/render/render_cube_camera.cpp
@@ -3,31 +3,17 @@
 namespace mos {
 
 RenderCubeCamera::RenderCubeCamera(const glm::vec3 &position,
-                                   const glm::vec2 &resolution) : position_(position),
-                                                                  resolution(resolution),
-                                                                  projection(glm::perspective(glm::pi<float>() / 2.0f,
-                                                                                              aspect_ratio(),
+                                   const glm::vec2 &resolution) : projection(glm::perspective(glm::pi<float>() / 2.0f,
+                                                                                              resolution.x / resolution.y,
                                                                                               0.1f,
-                                                                                              100.0f)) {
-  update_views();
-}
-glm::vec3 RenderCubeCamera::position() const {
-  return position_;
-}
-void RenderCubeCamera::position(const glm::vec3 &position) {
-  position_;
-  update_views();
-}
-float RenderCubeCamera::aspect_ratio() const {
-  return resolution.x / resolution.y;
-}
-void RenderCubeCamera::update_views() {
-  views[0] = glm::lookAt(position_, glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-  views[1] = glm::lookAt(position_, glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-  views[2] = glm::lookAt(position_, glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-  views[3] = glm::lookAt(position_, glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-  views[4] = glm::lookAt(position_, glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-  views[5] = glm::lookAt(position_, glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+                                                                                              100.0f)),
+                                                                  up(glm::vec3(0.0f, 0.0f, 1.0f)),
+    cameras{{mos::RenderCamera(position, position + glm::vec3(1.0f, 0.0f, 0.0f), projection, up, resolution),
+             mos::RenderCamera(position, position + glm::vec3(-11.0f, 0.0f, 0.0f), projection, up, resolution),
+             mos::RenderCamera(position, position + glm::vec3(0.0f, 1.0f, 0.0f), projection, up, resolution),
+             mos::RenderCamera(position, position + glm::vec3(0.0f, -1.0f, 0.0f), projection, up, resolution),
+             mos::RenderCamera(position, position + glm::vec3(0.0f, 0.0f, 1.0f), projection, up, resolution),
+             mos::RenderCamera(position, position + glm::vec3(0.0f, 0.0f, -1.0f), projection, up, resolution)}}
+    {}
 
-}
 }
