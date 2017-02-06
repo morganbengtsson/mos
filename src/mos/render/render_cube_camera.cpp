2592bd61838ea3858fdfa23566dd5260b22fe93f
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-02-06 16:47:37 +0100

    Checkpoint.

--- src/mos/render/render_cube_camera.cpp
+++ src/mos/render/render_cube_camera.cpp
@@ -5,7 +5,7 @@ namespace mos {
 RenderCubeCamera::RenderCubeCamera(const glm::vec3 &position,
                                    const glm::vec2 &resolution) : projection(glm::perspective(glm::half_pi<float>(),
                                                                                               resolution.x / resolution.y,
-                                                                                              0.01f,
+                                                                                              0.1f,
                                                                                               100.0f)),
                                                                   up(glm::vec3(0.0f, 0.0f, 1.0f)),
     cameras{{mos::RenderCamera(position, position + glm::vec3(1.0f, 0.0f, 0.0f), projection, glm::vec3(0.0f, 1.0f, 0.0f), resolution),
