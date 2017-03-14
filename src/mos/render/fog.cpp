06576faae9f2b4e25244797e08958729dafc2711
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-03-14 11:22:18 +0100

    Format fog class.

--- src/mos/render/fog.cpp
+++ src/mos/render/fog.cpp
@@ -2,12 +2,19 @@
 
 namespace mos {
 Fog::Fog(const glm::vec3 &color,
-                     const float near,
-                     const float far,
-                     const float linear_factor,
-                     const float exponential_factor,
-                     const float exponential_attenuation_factor,
-         const float exponential_power) : Fog(color, color, near, far, linear_factor, exponential_factor, exponential_attenuation_factor, exponential_power){}
+         const float near,
+         const float far,
+         const float linear_factor,
+         const float exponential_factor,
+         const float exponential_attenuation_factor,
+         const float exponential_power) : Fog(color,
+                                              color,
+                                              near,
+                                              far,
+                                              linear_factor,
+                                              exponential_factor,
+                                              exponential_attenuation_factor,
+                                              exponential_power) {}
 
 Fog::~Fog() {}
 Fog::Fog(const glm::vec3 &color_near,
@@ -17,9 +24,9 @@ Fog::Fog(const glm::vec3 &color_near,
          const float linear_factor,
          const float exponential_factor,
          const float exponential_attenuation_factor,
-         const float exponential_power) :color_near(color_near), color_far(color_far), near(near), far(far),
-linear_factor(linear_factor),
-exponential_factor(exponential_factor),
-exponential_power(exponential_power),
-exponential_attenuation_factor(exponential_attenuation_factor) {}
+         const float exponential_power) : color_near(color_near), color_far(color_far), near(near), far(far),
+                                          linear_factor(linear_factor),
+                                          exponential_factor(exponential_factor),
+                                          exponential_power(exponential_power),
+                                          exponential_attenuation_factor(exponential_attenuation_factor) {}
 }
