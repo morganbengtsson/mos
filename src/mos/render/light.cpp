0fb44cc7ee9aee0764121f6c86a7071caa57ed7f
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-03-15 16:36:02 +0100

    Checkpoint.

--- src/mos/render/light.cpp
+++ src/mos/render/light.cpp
@@ -13,7 +13,7 @@ Light::Light(const glm::vec3 &position,
     : diffuse(diffuse), specular(specular), angle_(angle),
       linear_attenuation_factor(linear_attenuation_factor),
       quadratic_attenuation_factor(quadratic_attenuation_factor),
-      camera(position, center, glm::perspective(angle, 1.0f, 0.01f, 10.0f),
+      camera(position, center, glm::perspective(angle, 1.0f, 1.0f, 50.0f),
                         glm::vec3(0.0f, 0.0001f, 1.0f)){}
 
 Light::~Light() {}
