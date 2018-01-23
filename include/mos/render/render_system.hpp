452abd609c8675d06451bb80c10eba3a98158b88
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-01-23 13:56:50 +0100

    Check

--- include/mos/render/render_system.hpp
+++ include/mos/render/render_system.hpp
@@ -86,7 +86,6 @@ public:
     clear(color);
     for (auto it = scenes_begin; it != scenes_end; it++) {
       render_shadow_map(*it);
-      //TODO: Render if no data in environment texture.
       render_environment(*it);
       render_scene(it->camera, *it, resolution);
     }
