0cd64c6e8659575002ecc389b844bf702a145d65
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-01-23 14:06:50 +0100

    Optimization bug

--- src/mos/render/environment_light.cpp
+++ src/mos/render/environment_light.cpp
@@ -6,14 +6,24 @@ EnvironmentLight::EnvironmentLight(const glm::vec3 &position,
                                    const glm::vec3 &extent,
                                    const float strength,
                                    const glm::uvec2 resolution)
-    : texture(resolution.x, resolution.y), box(RenderBox(position, extent)), strength(strength), cube_camera(position) {
+    : texture(resolution.x, resolution.y),
+      box(RenderBox(position, extent)),
+      strength(strength),
+      cube_camera(position) {
 }
-EnvironmentLight::EnvironmentLight(const EnvironmentLight &light) : texture(light.texture), box(light.box), strength(light.strength), cube_camera(light.cube_camera) {}
+EnvironmentLight::EnvironmentLight(const EnvironmentLight &light) :
+    texture(light.texture),
+    box(light.box),
+    strength(light.strength),
+    cube_camera(light.cube_camera),
+    target(light.target)
+    {}
 EnvironmentLight &EnvironmentLight::operator=(const EnvironmentLight &other) {
   texture = other.texture;
   box = other.box;
   strength = other.strength;
   cube_camera = other.cube_camera;
+  target = other.target;
   return *this;
 }
 
