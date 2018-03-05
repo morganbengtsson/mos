5761e4f4dd464c14dafa4c72fa9d739f7036cd7d
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-03-05 10:09:20 +0100

    Cleanup animation

--- include/mos/gfx/animation.hpp
+++ include/mos/gfx/animation.hpp
@@ -1,73 +1,44 @@
 #pragma once
 
+#include <mos/gfx/mesh.hpp>
 #include <initializer_list>
 #include <map>
 #include <memory>
 #include <utility>
-#include <mos/gfx/mesh.hpp>
 
 namespace mos {
 namespace gfx {
 
-/**
- * @brief Keyframe animation, interpolation between meshes.
- */
+/** Keyframe animation, interpolation between meshes. */
 class Animation {
 public:
   Animation();
-  /**
-   * @brief Animation default contructor
-   */
   Animation(const std::map<unsigned int, std::shared_ptr<Mesh const>> keyframes,
             const unsigned int frame_rate = 30);
-  /**
-   * @brief Animation constructor.
-   * @param keyframes Map of keys and shared Meshes
-   */
-  Animation(std::initializer_list<
+   Animation(std::initializer_list<
       std::pair<unsigned int, std::shared_ptr<Mesh const>>>
             keyframes,
             const unsigned int frame_rate = 30);
 
-  /**
-   *@brief Destructor.
-   */
   ~Animation();
 
   Animation(const std::string &path);
 
-  /**
-   * @brief update, move the animation forward.
-   * @param dt How much time to progress.
-   */
   void update(const float dt);
 
-  /**
-   * @brief Current frame
-   * @return frame
-   */
+  /** Current frame */
   unsigned int frame() const;
 
-  /**
-   * @brief reset, restart the animation.
-   */
+  /** Restart the animation. */
   void reset();
 
-  /**
-   * @brief Set frame rate.
-   */
+  /** Set frame rate. */
   void frame_rate(const unsigned int frame_rate);
 
-  /**
-   * @brief Frame rate in frames per second.
-   * @return
-   */
+  /** Frames per second. */
   unsigned int frame_rate() const;
 
-  /**
-   * @brief The mesh, which is animated.
-   * @return shared_ptr to mesh.
-   */
+  /** The mesh being animated. */
   std::shared_ptr<Mesh> mesh();
 
 private:
