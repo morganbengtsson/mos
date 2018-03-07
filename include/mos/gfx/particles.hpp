e8f3803e7f67e20ef28d510012e5d09070f92a21
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-03-07 09:09:01 +0100

    Clean up particles,.

--- include/mos/gfx/particles.hpp
+++ include/mos/gfx/particles.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <vector>
 #include <atomic>
 #include <mos/gfx/texture_2d.hpp>
@@ -13,123 +12,52 @@ namespace gfx {
 class Particles final {
 public:
   friend class Renderer;
-  /**
-   * @brief Particles constructor.
-   */
+  using Parts = std::vector<Particle>;
+
   Particles();
 
   template<class T>
-  /**
-   * @brief Particles constructor
-   * @param begin First particle iterator.
-   * @param end Last particle iterator.
-   */
   Particles(T begin, T end) {
     invalidate();
     particles_.assign(begin, end);
   }
 
-  /**
-   * @brief ~Particles destructor.
-   */
   virtual ~Particles();
 
-  /**
-   * @brief Particles container.
-   */
-  using Parts = std::vector<Particle>;
-
-  /**
-   * @brief begin
-   * @return Iterator to first particle.
-   */
   Parts::const_iterator begin() const { return particles_.begin(); }
-
-  /**
-   * @brief end
-   * @return Iterator to last particle.
-   */
   Parts::const_iterator end() const { return particles_.end(); }
-
-  /**
-   * @brief begin
-   * @return Iterator to first particle.
-   */
   Parts::iterator begin() { return particles_.begin(); }
-
-  /**
-   * @brief end
-   * @return Iterator last particle.
-   */
   Parts::iterator end() { return particles_.end(); }
 
   template<class T>
-  /**
-   * @brief Assign new particles.
-   * @param begin Iterator to first particle.
-   * @param end Iterator to last particle.
-   */
   void assign(T begin, T end) {
     particles_.assign(begin, end);
     invalidate();
   }
 
-  /**
-   * @brief Clear the container.
-   */
   void clear();
 
-  /**
-   * @brief Add a particle.
-   * @param particle A particle.
-   */
   void add(const Particle particle);
 
-  /**
-   * @brief back
-   * @return Last particle.
-   */
   Particle back();
 
-  /**
-   * @brief data
-   * @return Raw data.
-   */
   const Particle *data() const;
 
-  /**
-   * @brief Number of particles.
-   * @return Number of particles.
-   */
   unsigned int size() const;
 
-  /**
-   * @brief
-   * @return A unique identifier.
-   */
   unsigned int id() const;
 
-  /**
-   * @brief invalidate
-   */
   void invalidate();
 
-  /**
-   * @brief sort particles relative to a position
-   * @param position
-   */
+  /** Sort particles relative to a position. */
   void sort(const glm::vec3 &position);
 
   Particle &operator[](size_t index);
 
   const Particle &operator[](size_t index) const;
 
-  //TODO: Texture array for animation
-  /**
-   * @brief texture for quad
-   */
+  /** Texture for all particles. */
   SharedTexture2D emission_map;
-
 private:
   std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds> modified_;
   Parts particles_;
