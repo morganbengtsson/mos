bcc9aa2d2f19353e5d4eeecfec27090eb13cf02d
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-04-16 10:41:09 +0200

    Temp

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index.html'
+master_doc = 'index'
 
 # General information about the project.
 project = 'MOS'
