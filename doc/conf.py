a894583eb0b5daa1596da5ba67fb33779ebeb3f3
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-04-16 10:28:33 +0200

    Readme

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'README'
+master_doc = '../README'
 
 # General information about the project.
 project = 'MOS'
