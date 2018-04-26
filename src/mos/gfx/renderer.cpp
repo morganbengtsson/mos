09aafdd8829163e79073567b76c43fc20d75bcb3
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-04-26 16:10:43 +0200

    Check

--- src/mos/gfx/renderer.cpp
+++ src/mos/gfx/renderer.cpp
@@ -1064,7 +1064,9 @@ void Renderer::render_environment(const Scene &scene, const glm::vec4 &clear_col
                          GL_TEXTURE_CUBE_MAP_POSITIVE_X + cube_camera_index_, texture_id, 0);
   clear(clear_color);
   auto resolution = glm::vec2(scene.environment.texture_.width(), scene.environment.texture_.height());
-  render_scene(cube_camera, scene, resolution);
+  auto scene_copy = scene; // TODO: Nicer way to fix?
+  scene_copy.environment.strength = 0.0f;
+  render_scene(cube_camera, scene_copy, resolution);
 
   cube_camera_index_ = cube_camera_index_ >= 5 ? 0 : ++cube_camera_index_;
 
