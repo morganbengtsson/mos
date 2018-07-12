fa11e1c3970bbd17eda749b603bd708490d7a2e7
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-07-12 09:52:23 +0200

    Trigger

--- include/mos/exp/trigger.hpp
+++ include/mos/exp/trigger.hpp
@@ -11,19 +11,25 @@ public:
   Trigger(const T& value):
       value_(value),
       old_value_(value){}
+
   T operator=(const T& value){
     old_value_ = value_;
     value_ = value;
     return *this;
   }
+
   T operator-=(const T& value){
     *this = *this - value;
     return *this;
   }
-  bool changed() const{ return old_value_ != value_;};
+
   operator T() const {
     return value_;
   }
+
+  bool changed() const{ return old_value_ != value_;};
+
+  T old_value() const { return old_value_; };
 private:
   T value_;
   T old_value_;
