5fb8a8d4f3f9d303373095a8aae684a10b20bcb4
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-08-03 19:32:26 +0200

    Models.

--- include/mos/gfx/models.hpp
+++ include/mos/gfx/models.hpp
@@ -1,30 +1,13 @@
 #pragma once
 
-#include <initializer_list>
-#include <vector>
+#include <mos/core/container.hpp>
 
 namespace mos{
 namespace gfx{
 
 class Model;
 
-class Models {
-public:
-  using Items = std::vector<Model>;
-  template<class It>
-  Models(It begin, It end) : models_(begin, end){};
-  Models(const std::initializer_list<Model> &models);
-  Models();
-  void push_back(const Model& model);
-  Items::const_iterator begin() const;
-  Items::const_iterator end() const;
-  Items::iterator begin();
-  Items::iterator end();
-  Items::size_type size() const;
-  Model& operator[](unsigned long pos);
-private:
-  Items models_;
-};
+using Models = Container<Model>;
 
 }
 }
