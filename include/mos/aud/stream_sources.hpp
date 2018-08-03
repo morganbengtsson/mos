9924eedda9f9904a2deb3e44c23626f3e74a3ee6
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-08-03 20:41:08 +0200

    Stream sources container.

--- include/mos/aud/stream_sources.hpp
+++ include/mos/aud/stream_sources.hpp
@@ -1,27 +1,12 @@
 #pragma once
 
-#include <initializer_list>
-#include <vector>
+#include <mos/core/container.hpp>
 #include <mos/aud/stream_source.hpp>
 
 namespace mos {
 namespace aud {
 
-class StreamSources {
-public:
-  using Container = std::vector<StreamSource>;
-  StreamSources();
-  StreamSources(const std::initializer_list<StreamSource> &stream_sources);
-  template <class It>
-  StreamSources(It begin, It end) : stream_sources_(begin, end){}
-  Container::const_iterator begin() const;
-  Container::const_iterator end() const;
-  Container::iterator begin();
-  Container::iterator end();
-  void push_back(const StreamSource &stream_source);
-private:
-  Container stream_sources_;
-};
+using StreamSources = Container<StreamSource>;
 
 }
 }
