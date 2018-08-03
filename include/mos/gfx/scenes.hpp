d2ec28fa5ccab84964d64185fd3f4d86cf6701d2
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-08-03 19:33:46 +0200

    Scenes.

--- include/mos/gfx/scenes.hpp
+++ include/mos/gfx/scenes.hpp
@@ -1,27 +1,11 @@
 #pragma once
 
 #include <mos/gfx/scene.hpp>
-#include <initializer_list>
 
 namespace mos{
 namespace gfx{
 
-class Scenes {
-public:
-  using Container = std::vector<Scene>;
-  template<class It>
-  Scenes(It begin, It end) : scenes_(begin, end){};
-  Scenes(const std::initializer_list<Scene>& scenes);
-  Scenes();
-  Container::const_reference operator[](typename Container::size_type pos) const {
-    return scenes_[pos];
-  }
-  void push_back(const Scene& scene);
-  Container::const_iterator begin() const;
-  Container::const_iterator end() const;
-private:
-  std::vector<Scene> scenes_;
-};
+using Scenes = Container<Scene>;
 
 }
 }
