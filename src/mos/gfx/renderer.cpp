26e0dba9e2565c2fe092a695f1e8017f8ca87e04
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-08-09 13:16:32 +0200

    Dir

--- src/mos/gfx/renderer.cpp
+++ src/mos/gfx/renderer.cpp
@@ -1237,11 +1237,16 @@ Renderer::PropagateProgram::PropagateProgram() {
   auto vert_source = text("assets/shaders/" + name + ".vert");
   auto frag_source = text("assets/shaders/" + name + ".frag");
 
+  std::string functions_name = "functions";
+  std::string functions_frag_source = text("assets/shaders/" + functions_name + ".frag");
+
   const auto vertex_shader = Shader(vert_source, GL_VERTEX_SHADER, name);
   const auto fragment_shader = Shader(frag_source, GL_FRAGMENT_SHADER, name);
+  const auto functions_fragment_shader = Shader(functions_frag_source, GL_FRAGMENT_SHADER, functions_name);
 
   glAttachShader(program, vertex_shader.id);
   glAttachShader(program, fragment_shader.id);
+  glAttachShader(program, functions_fragment_shader.id);
   glBindAttribLocation(program, 0, "position");
   glBindAttribLocation(program, 1, "uv");
   link(name);
