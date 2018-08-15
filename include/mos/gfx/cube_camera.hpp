f7058f1f7b7a549c5d48485756408e5c6b957359
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-08-15 13:16:56 +0200

    Am clean up cube camera.

--- include/mos/gfx/cube_camera.hpp
+++ include/mos/gfx/cube_camera.hpp
@@ -11,16 +11,13 @@ namespace gfx {
 class CubeCamera {
 public:
   CubeCamera(const glm::vec3 &position = glm::vec3(0.0f, 0.0f, 1.25f),
-               const float near = 0.1,
-               const float far = 100.0f);
+             const float near = 0.1,
+             const float far = 100.0f);
 
   glm::vec3 position() const;
   void position(const glm::vec3 &position);
   void near_far(const float near, const float far);
   std::array<Camera, 6> cameras;
-private:
-  glm::mat4 projection_;
-  glm::vec3 up_;
 };
 }
 }
