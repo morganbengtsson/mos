7b6a9ddcd70449ceeb05dd0fd261b7d293bf9eaf
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-08-15 13:11:34 +0200

    Remove aspect cube camera.

--- src/mos/gfx/cube_camera.cpp
+++ src/mos/gfx/cube_camera.cpp
@@ -3,15 +3,13 @@
 #include <glm/gtc/matrix_transform.hpp>
 namespace mos {
 namespace gfx {
-CubeCamera::CubeCamera(const glm::vec3 &position,
-                       const float aspect_ratio,
-                       const float near,
-                       const float far) : projection_(glm::perspective(glm::half_pi<float>(),
-                                                                               aspect_ratio,
-                                                                               near,
-                                                                               far)),
-                                                   up_(glm::vec3(0.0f, 0.0f, 1.0f)),
-                                                   cameras{{Camera(position,
+CubeCamera::CubeCamera(const glm::vec3 &position, const float near, const float far)
+    : projection_(glm::perspective(glm::half_pi<float>(),
+                                   1.0f,
+                                   near,
+                                   far)),
+      up_(glm::vec3(0.0f, 0.0f, 1.0f)),
+      cameras{{Camera(position,
                                                                    position + glm::vec3(1.0f,
                                                                                         0.0f,
                                                                                         0.0f),
--- src/mos/gfx/environment_light.cpp
+++ src/mos/gfx/environment_light.cpp
@@ -9,7 +9,7 @@ EnvironmentLight::EnvironmentLight(const glm::vec3 &position,
     :
       box_(glm::translate(glm::mat4(1.0f), position), extent),
       strength(strength),
-      cube_camera_(position, 1.0f, 0.01f, glm::length(extent)) {
+      cube_camera_(position, 0.01f, glm::length(extent)) {
 }
 
 void EnvironmentLight::position(const glm::vec3 &position) {
