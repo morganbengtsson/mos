312821a541de5a075c82c00ae773e13a249cee15
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-08-16 09:17:09 +0200

    Swap interval and optimize.

--- src/mos/gfx/renderer.cpp
+++ src/mos/gfx/renderer.cpp
@@ -576,84 +576,85 @@ void Renderer::render_shadow_maps(const Models &models, const Lights &lights) {
 
 void Renderer::render_environment(const Scene &scene, const glm::vec4 &clear_color) {
   for (size_t i = 0; i < environment_maps_targets.size(); i++) {
-    GLuint frame_buffer_id = environment_maps_targets[i].frame_buffer;
-    glBindFramebuffer(GL_FRAMEBUFFER, frame_buffer_id);
+    if (scene.environment_lights[i].strength > 0.0f) {
+      GLuint frame_buffer_id = environment_maps_targets[i].frame_buffer;
+      glBindFramebuffer(GL_FRAMEBUFFER, frame_buffer_id);
 
-    auto texture_id = environment_maps_targets[i].texture;
+      auto texture_id = environment_maps_targets[i].texture;
 
-    auto cube_camera = scene.environment_lights[i].cube_camera_.cameras[cube_camera_index_[i]];
+      auto cube_camera = scene.environment_lights[i].cube_camera_.cameras[cube_camera_index_[i]];
 
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-                           GL_TEXTURE_CUBE_MAP_POSITIVE_X + cube_camera_index_[i], texture_id, 0);
-    clear(clear_color);
-    auto resolution = glm::ivec2(environment_render_buffer_.resolution,
-                                environment_render_buffer_.resolution);
+      glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+                             GL_TEXTURE_CUBE_MAP_POSITIVE_X + cube_camera_index_[i], texture_id, 0);
+      clear(clear_color);
+      auto resolution = glm::ivec2(environment_render_buffer_.resolution,
+                                   environment_render_buffer_.resolution);
 
-    glViewport(0, 0, resolution.x, resolution.y);
+      glViewport(0, 0, resolution.x, resolution.y);
 
-    glUseProgram(environment_program_.program);
+      glUseProgram(environment_program_.program);
 
-    glUniform1i(environment_program_.brdf_lut, 0);
-    glUniform1i(environment_program_.shadow_maps[0], 1);
-    glUniform1i(environment_program_.shadow_maps[1], 2);
+      glUniform1i(environment_program_.brdf_lut, 0);
+      glUniform1i(environment_program_.shadow_maps[0], 1);
+      glUniform1i(environment_program_.shadow_maps[1], 2);
 
-    glUniform1i(environment_program_.material_albedo_map, 3);
-    glUniform1i(environment_program_.material_emission_map, 4);
+      glUniform1i(environment_program_.material_albedo_map, 3);
+      glUniform1i(environment_program_.material_emission_map, 4);
 
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, brdf_lut_texture_.texture);
+      glActiveTexture(GL_TEXTURE0);
+      glBindTexture(GL_TEXTURE_2D, brdf_lut_texture_.texture);
 
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, shadow_maps_[0].texture);
+      glActiveTexture(GL_TEXTURE1);
+      glBindTexture(GL_TEXTURE_2D, shadow_maps_[0].texture);
 
-    glActiveTexture(GL_TEXTURE2);
-    glBindTexture(GL_TEXTURE_2D, shadow_maps_[1].texture);
+      glActiveTexture(GL_TEXTURE2);
+      glBindTexture(GL_TEXTURE_2D, shadow_maps_[1].texture);
 
-    // Camera in world space
-    auto position = cube_camera.position();
-    glUniform3fv(environment_program_.camera_position, 1, glm::value_ptr(position));
+      // Camera in world space
+      auto position = cube_camera.position();
+      glUniform3fv(environment_program_.camera_position, 1, glm::value_ptr(position));
 
-    for (size_t i = 0; i < scene.lights.size(); i++) {
-      glUniform3fv(environment_program_.lights[i].position, 1,
-                   glm::value_ptr(glm::vec3(glm::vec4(scene.lights[i].position(), 1.0f))));
+      for (size_t i = 0; i < scene.lights.size(); i++) {
+        glUniform3fv(environment_program_.lights[i].position, 1,
+                     glm::value_ptr(glm::vec3(glm::vec4(scene.lights[i].position(), 1.0f))));
 
-      glUniform3fv(environment_program_.lights[i].color, 1, glm::value_ptr(scene.lights[i].color));
-      glUniform1fv(environment_program_.lights[i].strength, 1, &scene.lights[i].strength);
+        glUniform3fv(environment_program_.lights[i].color, 1, glm::value_ptr(scene.lights[i].color));
+        glUniform1fv(environment_program_.lights[i].strength, 1, &scene.lights[i].strength);
 
-      glUniformMatrix4fv(environment_program_.lights[i].view, 1, GL_FALSE,
-                         &scene.lights[i].camera.view[0][0]);
-      glUniformMatrix4fv(environment_program_.lights[i].projection, 1, GL_FALSE,
-                         &scene.lights[i].camera.projection[0][0]);
+        glUniformMatrix4fv(environment_program_.lights[i].view, 1, GL_FALSE,
+                           &scene.lights[i].camera.view[0][0]);
+        glUniformMatrix4fv(environment_program_.lights[i].projection, 1, GL_FALSE,
+                           &scene.lights[i].camera.projection[0][0]);
 
-      auto light_angle = scene.lights[i].angle();
-      glUniform1fv(environment_program_.lights[i].angle, 1, &light_angle);
-      glUniform3fv(environment_program_.lights[i].direction, 1, glm::value_ptr(scene.lights[i].direction()));
+        auto light_angle = scene.lights[i].angle();
+        glUniform1fv(environment_program_.lights[i].angle, 1, &light_angle);
+        glUniform3fv(environment_program_.lights[i].direction, 1, glm::value_ptr(scene.lights[i].direction()));
+      }
+
+      glUniform2iv(environment_program_.camera_resolution, 1, glm::value_ptr(resolution));
+
+      glUniform3fv(environment_program_.fog_color_near, 1, glm::value_ptr(scene.fog.color_near));
+      glUniform3fv(environment_program_.fog_color_far, 1, glm::value_ptr(scene.fog.color_far));
+      glUniform1fv(environment_program_.fog_attenuation_factor, 1,
+                   &scene.fog.attenuation_factor);
+
+      for (auto &model : scene.models) {
+        render_model(model, glm::mat4(1.0f), cube_camera,
+                     scene.lights,
+                     scene.environment_lights,
+                     scene.fog,
+                     resolution, environment_program_);
+      }
+
+      cube_camera_index_[i] = cube_camera_index_[i] >= 5 ? 0 : ++cube_camera_index_[i]; //TODO PROBLEM
+
+      glBindFramebuffer(GL_FRAMEBUFFER, 0);
+      glBindTexture(GL_TEXTURE_CUBE_MAP, texture_id);
+      if (cube_camera_index_[i] == 0) {
+        glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
+      }
+      glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
     }
-
-    glUniform2iv(environment_program_.camera_resolution, 1, glm::value_ptr(resolution));
-
-    glUniform3fv(environment_program_.fog_color_near, 1, glm::value_ptr(scene.fog.color_near));
-    glUniform3fv(environment_program_.fog_color_far, 1, glm::value_ptr(scene.fog.color_far));
-    glUniform1fv(environment_program_.fog_attenuation_factor, 1,
-                 &scene.fog.attenuation_factor);
-
-    for (auto &model : scene.models) {
-      render_model(model, glm::mat4(1.0f), cube_camera,
-                   scene.lights,
-                   scene.environment_lights,
-                   scene.fog,
-                   resolution, environment_program_);
-    }
-
-
-    cube_camera_index_[i] = cube_camera_index_[i] >= 5 ? 0 : ++cube_camera_index_[i]; //TODO PROBLEM
-
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, texture_id);
-    if (cube_camera_index_[i] == 0) {
-      glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
-    }
-    glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
   }
 }
 void Renderer::load(const Mesh &mesh) {
