2c9162b9ee396daa2c93533ad4271d771e6a364b
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-08-17 10:52:47 +0200

    Missing.

---
+++ include/mos/gfx/particles.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <mos/core/tracked_container.hpp>
+#include <mos/gfx/particle.hpp>
+
+namespace mos {
+namespace gfx {
+
+using Particles = TrackedContainer<Particle>;
+
+}
+}
