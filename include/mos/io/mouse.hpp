0180bac17d6d91c4246c684ae66947f14c39ab86
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-12-12 14:32:07 +0100

    Events

--- include/mos/io/mouse.hpp
+++ include/mos/io/mouse.hpp
@@ -14,9 +14,10 @@ public:
     Button_press_0,
     Button_release_0,
   };
+  using Events = std::vector<Event>;
 
   glm::vec2 position;
-  std::vector<Event> events;
+  Events events;
   std::array<bool, 2> buttons;
 };
 
