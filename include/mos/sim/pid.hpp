ad4fb44bb2132c9218f12cda7f0be7eadc46246a
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-12-19 15:27:09 +0100

    Pid

--- include/mos/sim/pid.hpp
+++ include/mos/sim/pid.hpp
@@ -6,7 +6,7 @@ template<class T>
 /** Pid controller. */
 class Pid {
 public:
-  Pid(const T &error, const float Kp = 1.0f, const float Ki = 0.0f, const float Kd = 0.1f)
+  Pid(const T &error = T(0), const float Kp = 1.0f, const float Ki = 0.0f, const float Kd = 0.1f)
       : Kp(Kp), Ki(Ki), Kd(Kd), prev_error(error), P(error), I(error), D(error) {
   }
   T get(const T error, float dt) {
