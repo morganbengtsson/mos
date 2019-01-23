e8db1bb2396dedfb61e0f2169e94f2c30eb978e4
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2019-01-23 08:35:21 +0100

    Correction.

--- include/mos/gfx/cube_camera.hpp
+++ include/mos/gfx/cube_camera.hpp
@@ -11,7 +11,7 @@ namespace gfx {
 class Cube_camera final {
 public:
   explicit Cube_camera(const glm::vec3 &position = glm::vec3(0.0f, 0.0f, 1.25f),
-             float near = 0.1,
+             float near = 0.1f,
              float far = 100.0f);
 
   glm::vec3 position() const;
