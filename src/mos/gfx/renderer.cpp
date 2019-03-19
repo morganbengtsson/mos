7fc6effd68d9837253c9ce95d48dabf09d4d3202
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2019-03-19 14:51:58 +0100

    Check

--- src/mos/gfx/renderer.cpp
+++ src/mos/gfx/renderer.cpp
@@ -1050,7 +1050,7 @@ void Renderer::render(const Scenes &scenes, const glm::vec4 &color, const glm::i
   glUniform1i(bloom_program_.bloom_sampler_uniform, 1);
 
   glActiveTexture(GL_TEXTURE2);
-  glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, standard_target_.indirect_shading_texture);
+  glBindTexture(GL_TEXTURE_2D, ambient_occlusion_target_.texture);
   glUniform1i(bloom_program_.ambient_sampler_uniform, 2);
 
   float strength = 0.1f;
