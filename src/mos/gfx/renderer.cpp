a7e2fbf5f40ea6c341073df403c40bcb808f74c8
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2019-08-22 11:22:23 +0200

    Fix static initialization error.

--- src/mos/gfx/renderer.cpp
+++ src/mos/gfx/renderer.cpp
@@ -19,38 +19,42 @@
 namespace mos {
 namespace gfx {
 
-static const std::map<Texture::Wrap, GLint> wrap_map{
-    {Texture::Wrap::Clamp, GL_CLAMP_TO_EDGE},
-    {Texture::Wrap::Repeat, GL_REPEAT}};
 
-auto wrap_convert(const Texture::Wrap& w) { return wrap_map.at(w); }
 
-static const std::map<Texture::Filter, GLint> filter_map{
-    {Texture::Filter::Linear, GL_LINEAR},
-    {Texture::Filter::Closest, GL_NEAREST}};
+auto wrap_convert(const Texture::Wrap& w) {
+  static const std::map<Texture::Wrap, GLint> wrap_map{
+      {Texture::Wrap::Clamp, GL_CLAMP_TO_EDGE},
+      {Texture::Wrap::Repeat, GL_REPEAT}};
+  return wrap_map.at(w);
+}
 
-auto filter_convert(const Texture::Filter &f) { return filter_map.at(f); }
+auto filter_convert(const Texture::Filter &f) {
+  static const std::map<Texture::Filter, GLint> filter_map{
+      {Texture::Filter::Linear, GL_LINEAR},
+      {Texture::Filter::Closest, GL_NEAREST}};
+  return filter_map.at(f);
+}
 
-static const std::map<Texture::Filter, GLint> filter_map_mip{
-    {Texture::Filter::Linear, GL_LINEAR_MIPMAP_LINEAR},
-    {Texture::Filter::Closest, GL_NEAREST_MIPMAP_LINEAR}};
-
-auto filter_convert_mp(const Texture::Filter &f) { return filter_map_mip.at(f); }
+auto filter_convert_mip(const Texture::Filter &f) {
+  static const std::map<Texture::Filter, GLint> filter_map_mip{
+      {Texture::Filter::Linear, GL_LINEAR_MIPMAP_LINEAR},
+      {Texture::Filter::Closest, GL_NEAREST_MIPMAP_LINEAR}};
+  return filter_map_mip.at(f);
+}
 
 struct FormatPair {
   GLint internal_format;
   GLenum format;
 };
 
-static const std::map<Texture::Format, FormatPair> format_map{
-    {Texture::Format::R, {GL_RED, GL_RED}},
-    {Texture::Format::RG, {GL_RG, GL_RG}},
-    {Texture::Format::SRGB, {GL_SRGB, GL_RGB}},
-    {Texture::Format::SRGBA, {GL_SRGB_ALPHA, GL_RGBA}},
-    {Texture::Format::RGB, {GL_RGB, GL_RGB}},
-    {Texture::Format::RGBA, {GL_RGBA, GL_RGBA}}};
-
 auto format_convert(const Texture::Format &f){
+  static const std::map<Texture::Format, FormatPair> format_map{
+      {Texture::Format::R, {GL_RED, GL_RED}},
+      {Texture::Format::RG, {GL_RG, GL_RG}},
+      {Texture::Format::SRGB, {GL_SRGB, GL_RGB}},
+      {Texture::Format::SRGBA, {GL_SRGB_ALPHA, GL_RGBA}},
+      {Texture::Format::RGB, {GL_RGB, GL_RGB}},
+      {Texture::Format::RGBA, {GL_RGBA, GL_RGBA}}};
   return format_map.at(f);
 }
 
@@ -184,9 +188,9 @@ void Renderer::load_or_update(const Texture_2D &texture) {
     if (texture.layers.modified() > buffer->modified) {
       glBindTexture(GL_TEXTURE_2D, buffer->texture);
       glTexImage2D(GL_TEXTURE_2D, 0,
-                   format_map.at(texture.format).internal_format,
+                   format_convert(texture.format).internal_format,
                    texture.width(), texture.height(), 0,
-                   format_map.at(texture.format).format,
+                   format_convert(texture.format).format,
                    GL_UNSIGNED_BYTE, texture.layers[0].data());
       if (texture.mipmaps) {
         glGenerateMipmap(GL_TEXTURE_2D);
@@ -1758,12 +1762,12 @@ Renderer::Texture_buffer_2D::~Texture_buffer_2D() {
   glDeleteTextures(1, &texture);
 }
 Renderer::Texture_buffer_2D::Texture_buffer_2D(const Texture_2D &texture_2d)
-    : Texture_buffer_2D(format_map.at(texture_2d.format).internal_format,
-                        format_map.at(texture_2d.format).format,
+    : Texture_buffer_2D(format_convert(texture_2d.format).internal_format,
+                        format_convert(texture_2d.format).format,
                         texture_2d.width(), texture_2d.height(),
-                        texture_2d.mipmaps ? filter_map_mip.at(texture_2d.filter) : filter_map.at(texture_2d.filter),
-                        filter_map.at(texture_2d.filter),
-                        wrap_map.at(texture_2d.wrap),
+                        texture_2d.mipmaps ? filter_convert_mip(texture_2d.filter) : filter_convert(texture_2d.filter),
+                        filter_convert(texture_2d.filter),
+                        wrap_convert(texture_2d.wrap),
                         texture_2d.layers[0].data()) {}
 
 Renderer::Shader::Shader(const std::string &source,
