309d017e30fecf82dd387bf81940b781451f2612
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2020-02-11 16:39:39 +0100

    Missing files.

---
+++ include/mos/gfx/point_clouds.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <mos/core/container.hpp>
+#include <mos/gfx/point_cloud.hpp>
+
+namespace mos {
+namespace gfx {
+
+/** Collection of particle clouds.*/
+using Point_clouds = Container<Point_cloud>;
+
+}
+}
