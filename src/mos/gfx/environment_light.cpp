24723cf0358551af0a066b284c893ad437b52640
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2020-03-25 13:20:10 +0100

    Environment light near far import/export

--- src/mos/gfx/environment_light.cpp
+++ src/mos/gfx/environment_light.cpp
@@ -42,7 +42,10 @@ Environment_light::Environment_light(const std::string &directory, const std::st
     box_ = mos::gfx::Box(glm::translate(glm::mat4(1.0f), position), extent);
     strength = value.value("intensity", 1.0f);
     falloff = value.value("falloff", 0.1f);
-    cube_camera_ = mos::gfx::Cube_camera(position, 0.01f, glm::length(extent));
+    auto near = value.value("near", 0.01f);
+    auto far = value.value("far", glm::length(extent));
+
+    cube_camera_ = mos::gfx::Cube_camera(position, near, far);
   } else {
     throw std::runtime_error(path.substr(path.find_last_of('.')) +
         " file format is not supported.");
