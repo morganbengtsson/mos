6f8e6f1f5c97b02f9cd19bf5f96ae570899ad95a
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2020-04-06 13:00:53 +0200

    Move Cloud program.

--- include/mos/gfx/renderer.hpp
+++ include/mos/gfx/renderer.hpp
@@ -94,31 +94,8 @@ private:
 
   #include <mos/gfx/renderer/light_uniforms.hpp> //TODO: remove
 
+  #include <mos/gfx/renderer/cloud_program.hpp>
 
-  /** Uniforms for the particle shader program. */
-  struct Cloud_program : public Program {
-    explicit Cloud_program(const std::string &name, const Shader &functions_shader);
-    GLint model_view_projection;
-    GLint model_view;
-    GLint model{};
-    GLint projection;
-    GLint texture;
-    GLint resolution;    
-    GLint camera_resolution;
-    GLint camera_position;
-    GLint camera_far{};
-    GLint camera_near{};
-
-    struct Environment_uniforms {
-      GLint map;
-      GLint position;
-      GLint extent;
-      GLint strength;
-      GLint falloff;
-    };
-    std::array<Environment_uniforms, 2> environment_maps {};
-    std::array<Light_uniforms, 4> lights{};
-  };
 
   /** Uniforms for the bounding box shader program. */
   struct Box_program : public Program {
--- src/mos/gfx/renderer.cpp
+++ src/mos/gfx/renderer.cpp
@@ -1147,61 +1147,6 @@ Renderer::Standard_program::Standard_program(const Shader & functions_shader) {
   brdf_lut = glGetUniformLocation(program, "brdf_lut");
 }
 
-Renderer::Cloud_program::Cloud_program(const std::string & name, const Shader &functions_shader) {
-  std::string vert_source = text("assets/shaders/" + name + ".vert");
-  std::string frag_source = text("assets/shaders/" + name + ".frag");
-  const auto vertex_shader = Shader(vert_source, GL_VERTEX_SHADER, name);
-  const auto fragment_shader = Shader(frag_source, GL_FRAGMENT_SHADER, name);
-
-  glAttachShader(program, vertex_shader.id);
-  glAttachShader(program, fragment_shader.id);
-  glAttachShader(program, functions_shader.id);
-  glBindAttribLocation(program, 0, "position");
-  glBindAttribLocation(program, 1, "color");
-
-  link(name);
-  check(name);
-
-  glDetachShader(program, vertex_shader.id);
-  glDetachShader(program, fragment_shader.id);
-  glDetachShader(program, functions_shader.id);
-
-  model_view_projection = glGetUniformLocation(program, "model_view_projection");
-  model_view = glGetUniformLocation(program, "model_view");
-  projection = glGetUniformLocation(program, "projection");
-  texture = glGetUniformLocation(program, "texture_sampler");
-  resolution = glGetUniformLocation(program, "resolution");
-  camera_position = glGetUniformLocation(program, "camera.position");
-  camera_resolution = glGetUniformLocation(program, "camera.resolution");
-
-  for (size_t i = 0; i < lights.size(); i++) {
-    lights.at(i).position =
-        glGetUniformLocation(program, std::string("lights[" + std::to_string(i) + "].position").c_str());
-    lights.at(i).color = glGetUniformLocation(program, std::string("lights[" + std::to_string(i) + "].color").c_str());
-    lights.at(i).strength =
-        glGetUniformLocation(program, std::string("lights[" + std::to_string(i) + "].strength").c_str());
-    lights.at(i).view = glGetUniformLocation(program, std::string("lights[" + std::to_string(i) + "].view").c_str());
-    lights.at(i).projection =
-        glGetUniformLocation(program, std::string("lights[" + std::to_string(i) + "].projection").c_str());
-    lights.at(i).angle = glGetUniformLocation(program, std::string("lights[" + std::to_string(i) + "].angle").c_str());
-    lights.at(i).direction =
-        glGetUniformLocation(program, std::string("lights[" + std::to_string(i) + "].direction").c_str());
-
-  }
-  for (size_t i = 0; i < environment_maps.size(); i++) {
-    environment_maps.at(i).map =
-        glGetUniformLocation(program, std::string("environment_samplers[" + std::to_string(i) + "]").c_str());
-    environment_maps.at(i).position =
-        glGetUniformLocation(program, std::string("environments[" + std::to_string(i) + "].position").c_str());
-    environment_maps.at(i).extent =
-        glGetUniformLocation(program, std::string("environments[" + std::to_string(i) + "].extent").c_str());
-    environment_maps.at(i).strength =
-        glGetUniformLocation(program, std::string("environments[" + std::to_string(i) + "].strength").c_str());
-    environment_maps.at(i).falloff =
-        glGetUniformLocation(program, std::string("environments[" + std::to_string(i) + "].falloff").c_str());
-  }
-}
-
 Renderer::Box_program::Box_program() {
   std::string name = "box";
   std::string vert_source = text("assets/shaders/" + name + ".vert");
