535e2ad2fc531fda3bd3d32cd0d79f8ea985bcb7
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2020-04-13 12:17:23 +0200

    Experimental window event system.

--- include/mos/gfx/exp/window.hpp
+++ include/mos/gfx/exp/window.hpp
@@ -1,11 +1,34 @@
 #pragma once
 
+#include <GLFW/glfw3.h>
+#include <string>
+#include <mos/io/keyboard.hpp>
+#include <mos/io/mouse.hpp>
+
 namespace mos::io::exp{
 
 class Window
 {
 public:
-  Window();
+  struct Output {
+    Keyboard keyboard;
+  }
+  Window(const std::string &title = "Window",
+        const glm::ivec2 &resolution = {1920, 1080},
+         int swap_interval = 0);
+  ~Window();
+  Output poll_events();
+  void swap_buffers();
+private:
+  GLFWwindow *window_;
+  Keyboard keyboard_;
+  Mouse mouse_;
+  void error_callback(int error, const char* description);
+  void key_callback(int key, int scancode, int action, int mods);
+  void char_callback(const unsigned int codepoint);
+  void cursor_position_callback(double xpos, double ypos);
+  void mouse_button_callback(int button, int action, int mods);
 };
+
 
 }
--- src/mos/gfx/exp/window.cpp
+++ src/mos/gfx/exp/window.cpp
@@ -1,5 +1,128 @@
 #include <mos/gfx/exp/window.hpp>
+#include <glad/glad.h>
 
 namespace mos::io::exp {
-Window::Window() {}
+
+Window::Window(const std::string &title,
+               const glm::ivec2 &resolution,
+               int swap_interval){
+  if (!glfwInit()) {
+    exit(EXIT_FAILURE);
+  }
+
+
+  glfwWindowHint(GLFW_SRGB_CAPABLE, true);
+  glfwWindowHint(GLFW_RESIZABLE, false);
+
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+
+  window_ = glfwCreateWindow(resolution.x, resolution.y, title.c_str(), nullptr,
+                             nullptr);
+  glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
+
+  if (!window_) {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glfwMakeContextCurrent(window_);
+
+  gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+
+  glfwSetKeyCallback(window_, key_callback);
+  glfwSetWindowSizeCallback(window_, size_callback);
+  glfwSetMouseButtonCallback(window_, click_callback);
+  glfwSetCursorPosCallback(window_, mouse_callback);
+  glfwSetScrollCallback(window_, scroll_callback);
+  glfwSetWindowPosCallback(window_, position_callback);
+  glfwSetCharCallback(window_, char_callback);
+
+  glfwSwapInterval(swap_interval);
+
+  glfwSetErrorCallback(error_callback);
+  glfwSetKeyCallback(window_, key_callback);
+  glfwSetCharCallback(window_, char_callback);
+  glfwSetCursorPosCallback(window_, cursor_position_callback);
+  glfwSetMouseButtonCallback(window_, mouse_button_callback);
 }
+
+Window::~Window(){
+  glfwDestroyWindow(window_);
+}
+
+Window::Output Window::poll_events() {
+  keyboard_.events.clear();
+  keyboard_.codepoints.clear();
+  mouse_.events.clear();
+
+  glfwPollEvents();
+
+  Output output{keyboard_, mouse_};
+  return output;
+}
+
+void Window::error_callback(int error, const char *description) {
+  fprintf(stderr, "Error: %s\n", description);
+}
+
+void Window::key_callback(int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_ENTER) {
+    if (action == GLFW_PRESS) {
+      keyboard_.events.insert(mos::io::Keyboard::Event::Button_enter_press);
+    }
+    else if (action == GLFW_RELEASE){
+      keyboard_.events.insert(mos::io::Keyboard::Event::Button_enter_release);
+    }
+  }
+
+  if (key == GLFW_KEY_BACKSPACE) {
+    if (action == GLFW_PRESS) {
+      keyboard_.events.insert(mos::io::Keyboard::Event::Button_backspace_press);
+    }
+    else if (action == GLFW_RELEASE){
+      keyboard_.events.insert(mos::io::Keyboard::Event::Button_backspace_release);
+    }
+    }
+}
+
+void Window::char_callback(const unsigned int codepoint) {
+  keyboard_.codepoints.push_back(codepoint);
+}
+
+void Window::cursor_position_callback(double xpos, double ypos) {
+  mouse_.old_position = mouse_.position;
+  mouse_.position = glm::dvec2(xpos, ypos);
+}
+
+void Window::mouse_button_callback(int button, int action, int mods) {
+  if (button == GLFW_MOUSE_BUTTON_1 && action == GLFW_PRESS) {
+    mouse_.buttons[0] = true;
+    mouse_.events.insert(mos::io::Mouse::Event::Button_press_0);
+  }
+  if (button == GLFW_MOUSE_BUTTON_1 && action == GLFW_RELEASE) {
+    mouse_.buttons[0] = false;
+    mouse_.events.insert(mos::io::Mouse::Event::Button_release_0);
+  }
+  if (button == GLFW_MOUSE_BUTTON_2 && action == GLFW_PRESS) {
+    mouse_.buttons[1] = true;
+    mouse_.events.insert(mos::io::Mouse::Event::Button_press_1);
+  }
+  if (button == GLFW_MOUSE_BUTTON_2 && action == GLFW_RELEASE) {
+    mouse_.buttons[1] = false;
+    mouse_.events.insert(mos::io::Mouse::Event::Button_release_1);
+  }
+
+  if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_PRESS) {
+    mouse_.buttons[2] = true;
+  }
+  if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_RELEASE) {
+    mouse_.buttons[2] = false;
+  }
+}
+}
+
