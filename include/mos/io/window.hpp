71390e12603dad48c531e6f5a79923a64009aa65
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2020-04-14 12:55:50 +0200

    Replace io window api.

--- include/mos/io/window.hpp
+++ include/mos/io/window.hpp
@@ -1,85 +1,37 @@
 #pragma once
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <functional>
-#include <glm/glm.hpp>
 #include <string>
+#include <glm/glm.hpp>
+#include <mos/io/keyboard.hpp>
+#include <mos/io/mouse.hpp>
 
-namespace mos {
-namespace io {
+namespace mos::io {
 
 class Window {
 public:
-  enum class Cursor_mode : int {
-    Normal = GLFW_CURSOR_NORMAL,
-    Hidden = GLFW_CURSOR_HIDDEN,
-    Disabled = GLFW_CURSOR_DISABLED
+  struct Output {
+    Keyboard keyboard;
+    Mouse mouse;
   };
-
-  enum class Cursor {
-    Hand, Arrow, Crosshair
-  };
-
-  Window(const std::string &title = "Window", const glm::ivec2 &resolution = {1920, 1080}, int swap_interval = 0);
+  Window(const std::string &title = "Window",
+        const glm::ivec2 &resolution = {1920, 1080},
+         int swap_interval = 0);
   ~Window();
-  Window(const Window & window) = delete;
-  Window(const Window && window) = delete;
-  Window & operator=(const Window & window) = delete;
-  Window & operator=(const Window && window) = delete;
-
-  using Error_func =
-  std::function<void(const int error, const std::string &description)>;
-  using Pos_func = std::function<void(const glm::vec2 &position)>;
-  using Size_func = std::function<void(const glm::vec2 &size)>;
-  using Click_func =
-  std::function<void(const int button, const int action, const int mods)>;
-  using Mouse_func = std::function<void(const glm::dvec2 &position)>;
-  using Scroll_func = std::function<void(const glm::dvec2 &offset)>;
-  using Key_func = std::function<void(const int key, const int scancode,
-                                     const int action, const int mods)>;
-  using Char_func = std::function<void(const unsigned int codepoint)>;
-
-  static Error_func error_func;
-  static Pos_func pos_func;
-  static Size_func size_func;
-  static Click_func click_func;
-  static Mouse_func mouse_func;
-  static Scroll_func scroll_func;
-  static Key_func key_func;
-  static Char_func char_func;
-
-  glm::ivec2 framebuffer_size() const;
-  void poll_events();
+  static Output poll_events();
   void swap_buffers();
-  bool close() const;
-  void close(bool close);
-  void cursor_mode(const Cursor_mode &mode);
-  void cursor(const Cursor &cursor);
-  glm::dvec2 cursor_position() const;
   float dpi() const;
-
+  bool close() const;
+  void close(const bool close);
 private:
   GLFWwindow *window_;
-  GLFWcursor *hand_cursor_;
-  GLFWcursor *arrow_cursor_;
-  GLFWcursor *crosshair_cursor_;
-
-  static void error_callback(int error, const char *description);
-  static void position_callback(GLFWwindow *window, int x, int y);
-  static void size_callback(GLFWwindow *window, int width, int height);
-  static void click_callback(GLFWwindow *window, int button, int action,
-                             int mods);
-  static void mouse_callback(GLFWwindow *window, double x, double y);
-  static void scroll_callback(GLFWwindow *window, double xoffset,
-                              double yoffset);
-  static void key_callback(GLFWwindow *window, int key, int scancode,
-                           int action, int mods);
-  static void char_callback(GLFWwindow * window, unsigned int codepoint);
-
-  bool pause_ = false;
-  float pause_time_ = 0.0f;
-  const float max_pause_ = 0.2f;
+  static Keyboard keyboard_;
+  static Mouse mouse_;
+  static void error_callback(int error, const char* description);
+  static void key_callback(GLFWwindow * window, int key, int scancode, int action, int mods);
+  static void char_callback(GLFWwindow * window, const unsigned int codepoint);
+  static void cursor_position_callback(GLFWwindow * window, double xpos, double ypos);
+  static void mouse_button_callback(GLFWwindow * window, int button, int action, int mods);
 };
 }
-}
-
--- src/mos/io/window.cpp
+++ src/mos/io/window.cpp
@@ -1,29 +1,20 @@
-#include <iostream>
 #include <mos/io/window.hpp>
 
-namespace mos {
-namespace io {
+namespace mos::io {
 
-Window::Error_func Window::error_func;
-Window::Pos_func Window::pos_func;
-Window::Size_func Window::size_func;
-Window::Click_func Window::click_func;
-Window::Mouse_func Window::mouse_func;
-Window::Scroll_func Window::scroll_func;
-Window::Key_func Window::key_func;
-Window::Char_func Window::char_func;
+Keyboard Window::keyboard_ = Keyboard();
+Mouse Window::mouse_ = Mouse();
 
-Window::Window(const std::string &title, const glm::ivec2 &resolution, const int swap_interval) {
+Window::Window(const std::string &title,
+               const glm::ivec2 &resolution,
+               int swap_interval){
   if (!glfwInit()) {
     exit(EXIT_FAILURE);
   }
-  glfwSetErrorCallback(error_callback);
 
-  //glfwWindowHint(GLFW_SAMPLES, 8);
   glfwWindowHint(GLFW_SRGB_CAPABLE, true);
   glfwWindowHint(GLFW_RESIZABLE, false);
 
-  // Context
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -34,8 +25,6 @@ Window::Window(const std::string &title, const glm::ivec2 &resolution, const int
                              nullptr);
   glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
 
-  // glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
   if (!window_) {
     glfwTerminate();
     exit(EXIT_FAILURE);
@@ -45,115 +34,31 @@ Window::Window(const std::string &title, const glm::ivec2 &resolution, const int
 
   gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
 
-  glfwSetKeyCallback(window_, key_callback);
-  glfwSetWindowSizeCallback(window_, size_callback);
-  glfwSetMouseButtonCallback(window_, click_callback);
-  glfwSetCursorPosCallback(window_, mouse_callback);
-  glfwSetScrollCallback(window_, scroll_callback);
-  glfwSetWindowPosCallback(window_, position_callback);
-  glfwSetCharCallback(window_, char_callback);
-
   glfwSwapInterval(swap_interval);
 
-  hand_cursor_ = glfwCreateStandardCursor(GLFW_HAND_CURSOR);
-  arrow_cursor_ = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
-  crosshair_cursor_ = glfwCreateStandardCursor(GLFW_CROSSHAIR_CURSOR);
-
-  error_func = [](int error, const std::string &description) {
-    fputs(description.c_str(), stderr);
-  };
+  glfwSetErrorCallback(error_callback);
+  glfwSetKeyCallback(window_, key_callback);
+  glfwSetCharCallback(window_, char_callback);
+  glfwSetCursorPosCallback(window_, cursor_position_callback);
+  glfwSetMouseButtonCallback(window_, mouse_button_callback);
 }
 
-Window::~Window() {
-  glfwDestroyCursor(hand_cursor_);
-  glfwDestroyCursor(arrow_cursor_);
+Window::~Window(){
   glfwDestroyWindow(window_);
 }
 
-void Window::error_callback(int error, const char *description) {
-  if (error_func) {
-    error_func(error, std::string(description));
-  }
+void Window::swap_buffers() {
+  glfwSwapBuffers(window_);
 }
 
-void Window::position_callback(GLFWwindow *window, int x, int y) {
-  if (pos_func) {
-    pos_func(glm::ivec2(x, y));
-  }
-}
+Window::Output Window::poll_events() {
+  keyboard_.events.clear();
+  keyboard_.codepoints.clear();
+  mouse_.events.clear();
 
-void Window::size_callback(GLFWwindow *window, int width, int height) {
-  if (size_func) {
-    size_func(glm::ivec2(width, height));
-  }
-}
+  glfwPollEvents();
 
-void Window::click_callback(GLFWwindow *window, int button, int action,
-                            int mods) {
-  if (click_func) {
-    click_func(button, action, mods);
-  }
-}
-
-void Window::mouse_callback(GLFWwindow *window, double x, double y) {
-  if (mouse_func) {
-    mouse_func(glm::dvec2(x, y));
-  }
-}
-
-void Window::scroll_callback(GLFWwindow *window, double xoffset,
-                             double yoffset) {
-  if (scroll_func) {
-    scroll_func(glm::dvec2(xoffset, yoffset));
-  }
-}
-void Window::key_callback(GLFWwindow *window, int key, int scancode, int action,
-                          int mods) {
-  if (key_func) {
-    key_func(key, scancode, action, mods);
-  }
-}
-
-void Window::char_callback(GLFWwindow *window, unsigned int codepoint){
-  if(char_func){
-    char_func(codepoint);
-  }
-}
-
-glm::ivec2 Window::framebuffer_size() const {
-  int width, height;
-  glfwGetFramebufferSize(window_, &width, &height);
-  return glm::ivec2(width, height);
-}
-
-void Window::poll_events() { glfwPollEvents(); }
-
-void Window::swap_buffers() { glfwSwapBuffers(window_); }
-
-bool Window::close() const { return glfwWindowShouldClose(window_); }
-void Window::close(const bool close) {
-  glfwSetWindowShouldClose(window_, close);
-}
-
-void Window::cursor_mode(const Window::Cursor_mode &mode) {
-  glfwSetInputMode(window_, GLFW_CURSOR, static_cast<int>(mode));
-}
-
-void Window::cursor(const Window::Cursor &cursor) {
-  if (cursor == Cursor::Hand) {
-    glfwSetCursor(window_, hand_cursor_);
-  } else if (cursor == Cursor::Arrow){
-    glfwSetCursor(window_, arrow_cursor_);
-  }
-  else if (cursor == Cursor::Crosshair){
-    glfwSetCursor(window_, crosshair_cursor_);
-  }
-}
-
-glm::dvec2 Window::cursor_position() const {
-  double xpos, ypos;
-  glfwGetCursorPos(window_, &xpos, &ypos);
-  return glm::dvec2(xpos, ypos);
+  return Output{keyboard_, mouse_};
 }
 
 float Window::dpi() const {
@@ -164,5 +69,35 @@ float Window::dpi() const {
   const double dpi = mode->width / (widthMM / 25.4);
   return dpi;
 }
+
+bool Window::close() const {
+  return glfwWindowShouldClose(window_);
+}
+
+void Window::close(const bool close) {
+  glfwSetWindowShouldClose(window_, close);
+}
+
+void Window::error_callback(int error, const char *description) {
+  fprintf(stderr, "Error: %s\n", description);
+}
+
+void Window::key_callback(GLFWwindow * window, int key, int scancode, int action, int mods) {
+  keyboard_.events.insert({static_cast<Keyboard::Key>(key), static_cast<Keyboard::Action>(action)});
+}
+
+void Window::char_callback(GLFWwindow * window, const unsigned int codepoint) {
+  keyboard_.codepoints.push_back(codepoint);
+}
+
+void Window::cursor_position_callback(GLFWwindow * window, double xpos, double ypos) {
+  mouse_.old_position = mouse_.position;
+  mouse_.position = glm::dvec2(xpos, ypos);
+}
+
+void Window::mouse_button_callback(GLFWwindow * window, int button, int action, int mods) {
+  mouse_.buttons[button] = static_cast<bool>(action);
+  mouse_.events.insert({static_cast<Mouse::Button>(button), static_cast<Mouse::Action>(action)});
 }
 }
+
