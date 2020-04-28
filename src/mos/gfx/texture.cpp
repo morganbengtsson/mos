03da036be0816d499af801d31ed5dd98ce4dd37b
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2020-04-28 17:10:54 +0200

    Tidy for texture.

--- src/mos/gfx/texture.cpp
+++ src/mos/gfx/texture.cpp
@@ -3,12 +3,11 @@
 #include <stb_image.h>
 #include <map>
 
-namespace mos {
-namespace gfx {
+namespace mos::gfx {
 std::atomic_int Texture::current_id_;
 Texture::Texture(const int width, const int height,
-                 const Texture::Format &format, const Texture::Filter &filter,
-                 const Texture::Wrap &wrap, const bool mipmaps)
+                 const Format &format, const Filter &filter,
+                 const Wrap &wrap, const bool mipmaps)
     : mipmaps(mipmaps), filter(filter), wrap(wrap),
       format(format), layers{Data()}, id_(current_id_++), width_(width),
       height_(height) {}
@@ -16,9 +15,9 @@ Texture::Texture(const int width, const int height,
 Texture::Texture(const std::initializer_list<Texture::Data> &layers,
                  const int width,
                  const int height,
-                 const Texture::Format &format,
-                 const Texture::Filter &filter,
-                 const Texture::Wrap &wrap,
+                 const Format &format,
+                 const Filter &filter,
+                 const Wrap &wrap,
                  const bool mipmaps) : Texture(layers.begin(),
                                                layers.end(),
                                                width,
@@ -29,31 +28,29 @@ Texture::Texture(const std::initializer_list<Texture::Data> &layers,
                                                mipmaps) {}
 
 Texture::Texture(const std::initializer_list<std::string> &paths,
-                 const bool color_data, const Texture::Filter &filter,
+                 const bool color_data, const Filter &filter,
                  const Texture::Wrap &wrap, const bool mipmaps)
     : mipmaps(mipmaps), filter(filter), wrap(wrap), id_(current_id_++),
       width_(0), height_(0) {
+  std::map<int, Format> bpp_map{{1, Format::R}, {2, Format::RG}, {3, color_data ? Format::SRGB : Format::RGB}, {4, color_data ? Format::SRGBA : Format::RGBA}};
   for (auto &path : paths) {
     int bpp;
     unsigned char *pixels = stbi_load(path.c_str(), &width_, &height_, &bpp, 0);
     layers.push_back(Data(pixels, pixels + (width_ * height_ * bpp)));
-    std::map<int, Format> bpp_map{{1, Format::R}, {2, Format::RG}, {3, color_data ? Format::SRGB : Format::RGB}, {4, color_data ? Format::SRGBA : Format::RGBA}};
     format = bpp_map[bpp];
   }
 }
 
-int Texture::id() const {
+auto Texture::id() const -> int {
   return id_;
 }
-int Texture::width() const {
+auto Texture::width() const -> int {
   return width_;
 }
-int Texture::height() const {
+auto Texture::height() const -> int {
   return height_;
 }
-Tracked_container<Texture::Data>::size_type Texture::depth() const {
+auto Texture::depth() const -> Tracked_container<Data>::size_type {
   return layers.size();
-}
-
 }
 }
