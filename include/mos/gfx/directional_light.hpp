dee6171db279831d04954cf82154c2d16733721a
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2020-07-13 15:36:08 +0200

    Import directional light.

--- include/mos/gfx/directional_light.hpp
+++ include/mos/gfx/directional_light.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <string>
 
 namespace mos::gfx {
 
 struct Directional_light {
+  Directional_light() = default;
+  Directional_light(const std::string &directory,
+                    const std::string &path,
+                    const glm::mat4 &parent_transform = glm::mat4(1.0f));
   glm::vec3 position{0.0f};
   glm::vec3 direction{0.0f, 0.0f, 1.0f};
   glm::vec3 color{1.0f};
