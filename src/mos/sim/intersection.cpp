bc407da2679a54472e9009e5b3beda6dfc83982d
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2021-06-19 14:35:53 +0200

    Remove deprecated classes

--- src/mos/sim/intersection.cpp
+++
@@ -1,7 +0,0 @@
-#include <mos/sim/intersection.hpp>
-
-namespace mos::sim {
-Intersection::Intersection(const glm::vec3 &position, const glm::vec3 &normal)
-    : position(position), normal(normal) {}
-
-}
