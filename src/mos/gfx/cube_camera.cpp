bda7b69faf52a863678bdf0a540781037c9195a8
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2021-06-19 15:04:04 +0200

    Refactor cube camera

--- src/mos/gfx/cube_camera.cpp
+++ src/mos/gfx/cube_camera.cpp
@@ -3,73 +3,51 @@
 
 namespace mos::gfx {
 
-Cube_camera::Cube_camera(const glm::vec3 &position, const float near, const float far) :
-    cameras{{Camera(position,
-                    position + glm::vec3(1.0f,
-                                         0.0f,
-                                         0.0f),
-                    glm::perspective(glm::half_pi<float>(),
-                                     1.0f,
-                                     near,
-                                     far),
-                    glm::vec3(0.0f, -1.0f, 0.0f)),
-             Camera(position,
-                    position + glm::vec3(-1.0f,
-                                         0.0f,
-                                         0.0f),
-                    cameras[0].projection(),
-                    glm::vec3(0.0f, -1.0f, 0.0f)),
-             Camera(position,
-                    position + glm::vec3(0.0f,
-                                         1.0f,
-                                         0.0f),
-                    cameras[0].projection(),
-                    glm::vec3(0.0f, 0.0f, 1.0f)),
-             Camera(position,
-                    position + glm::vec3(0.0f,
-                                         -1.0f,
-                                         0.0f),
-                    cameras[0].projection(),
-                    glm::vec3(0.0f, 0.0f, -1.0f)),
-             Camera(position,
-                    position + glm::vec3(0.0f,
-                                         0.0f,
-                                         1.0f),
-                    cameras[0].projection(),
-                    glm::vec3(0.0f, -1.0f, 0.0f)),
-             Camera(position,
-                    position + glm::vec3(0.0f,
-                                         0.0f,
-                                         -1.0f),
-                    cameras[0].projection(),
-                    glm::vec3(0.0f,
-                              -1.0f,
-                              0.0f))}} {}
+namespace  {
+
+const glm::vec3 x_axis{1.0f, 0.0f, 1.0f};
+const glm::vec3 y_axis{0.0f, 1.0f, 0.0f};
+const glm::vec3 z_axis{0.0f, 0.0f, 1.0f};
+
+} // namespace
+
+Cube_camera::Cube_camera(const glm::vec3 &position, const float near,
+                         const float far)
+    : cameras{
+          {Camera(position, position + x_axis,
+                  glm::perspective(glm::half_pi<float>(), 1.0f, near, far),
+                  -y_axis),
+           Camera(position, position - x_axis, cameras[0].projection(),
+                  -y_axis),
+           Camera(position, position + y_axis, cameras[0].projection(), z_axis),
+           Camera(position, position - y_axis, cameras[0].projection(),
+                  -z_axis),
+           Camera(position, position + z_axis, cameras[0].projection(),
+                  -y_axis),
+           Camera(position, position - z_axis, cameras[0].projection(),
+                  -y_axis)}} {}
 auto Cube_camera::position() const -> glm::vec3 {
   return cameras[0].position();
 }
 
 void Cube_camera::position(const glm::vec3 &position) {
   cameras[0].position(position);
-  cameras[0].direction(glm::vec3(1.0f, 0.0f, 0.0f));
+  cameras[0].direction(x_axis);
   cameras[1].position(position);
-  cameras[1].direction(glm::vec3(-1.0f, 0.0f, 0.0f));
+  cameras[1].direction(-x_axis);
   cameras[2].position(position);
-  cameras[2].direction(glm::vec3(0.0f, 1.0f, 0.0f));
+  cameras[2].direction(y_axis);
   cameras[3].position(position);
-  cameras[3].direction(glm::vec3(0.0f, -1.0f, 0.0f));
+  cameras[3].direction(-y_axis);
   cameras[4].position(position);
-  cameras[4].direction(glm::vec3(0.0f, 0.0f, 1.0f));
+  cameras[4].direction(z_axis);
   cameras[5].position(position);
-  cameras[5].direction(glm::vec3(0.0f, 0.0f, -1.0f));
+  cameras[5].direction(-z_axis);
 }
 
 void Cube_camera::near_far(const float near, const float far) {
   for (auto &camera : cameras) {
-    camera.projection(glm::perspective(glm::half_pi<float>(),
-                                         1.0f,
-                                         near,
-                                         far));
+    camera.projection(glm::perspective(glm::half_pi<float>(), 1.0f, near, far));
   }
 }
-}
+} // namespace mos::gfx
