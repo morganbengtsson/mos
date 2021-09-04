51ecfdfa1970cae2a0c4992867878c9cfa3c54d5
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2021-09-04 11:18:53 +0200

    Texture class

---
+++ src/mos/gfx/renderer/texture_2d.cpp
@@ -0,0 +1,4 @@
+#include "texture_2d.hpp"
+
+Texture_2D::Texture_2D(const mos::gfx::Texture_2D &texture)
+    : id(texture.id()) {}
---
+++ src/mos/gfx/renderer/texture_2d.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <mos/gfx/texture_2d.hpp>
+
+class Texture_2D final {
+  friend class Renderer;
+
+private:
+  Texture_2D(const mos::gfx::Texture_2D &texture);
+
+  const int id;
+};
