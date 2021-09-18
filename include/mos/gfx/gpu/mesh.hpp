d0df69f1d07392696af3201fabb96599d50adb8e
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2021-09-18 10:33:39 +0200

    Move definitions

--- include/mos/gfx/gpu/mesh.hpp
+++ include/mos/gfx/gpu/mesh.hpp
@@ -3,21 +3,25 @@
 #include <mos/gfx/mesh.hpp>
 #include <mos/gfx/gpu/resource.hpp>
 
+namespace mos::gfx::gl {
+class Renderer;
+}
+
 namespace mos::gfx::gpu {
 
 class Mesh final : public Resource {
   friend class Model;
-
+  friend class mos::gfx::gl::Renderer;
 public:
   Mesh();
   auto centroid() const -> glm::vec3;
   auto radius() const -> float;
   auto num_indices() const -> int;
 
+private:
 
   Mesh(const mos::gfx::Mesh& mesh);
   Mesh(const mos::gfx::Shared_mesh &shared_mesh);
-private:
   glm::vec3 centroid_{0.0f};
   float radius_{0.0f};
   int num_indices_{0};
--- src/mos/gfx/gpu/model.cpp
+++ src/mos/gfx/gpu/model.cpp
@@ -1,5 +1,29 @@
 #include <mos/gfx/gpu/model.hpp>
 
-glm::vec3 mos::gfx::gpu::Model::position() const {
+namespace mos::gfx::gpu {
+
+Model::Model(mos::gfx::Model model)
+    : mesh(model.mesh), material(model.material), transform(model.transform) {
+  for (auto model : model.models) {
+    models.push_back(Model(model));
+  }
+}
+
+auto Model::centroid() const -> glm::vec3{
+  return (transform * glm::translate(glm::mat4(1.0f), mesh.centroid()))[3];
+}
+
+auto Model::radius() const -> float {
+  glm::vec3 scale;
+  glm::quat rotation;
+  glm::vec3 translation;
+  glm::vec3 skew;
+  glm::vec4 perspective;
+  glm::decompose(transform, scale, rotation, translation, skew, perspective);
+  return mesh.radius() * glm::max(glm::max(scale.x, scale.y), scale.z);
+}
+
+auto Model::position() const -> glm::vec3 {
   return glm::vec3(transform[3]);
+}
 }
