a5bfa79741368ccfe6174ff2c163d49560ac8b55
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2021-09-25 12:23:14 +0200

    Refactor

--- include/mos/al/buffer.hpp
+++ include/mos/al/buffer.hpp
@@ -19,38 +19,17 @@ public:
 
 public:
   Buffer(const Buffer &buffer) = delete;
-  Buffer(Buffer &&buffer) noexcept : id(buffer.id) {
-    buffer.id = 0;
-  }
+  Buffer(Buffer &&buffer) noexcept;
 
   Buffer &operator=(const Buffer &buffer) = delete;
-  Buffer &operator=(Buffer &&buffer) noexcept {
-    if (this != &buffer) {
-      release();
-      std::swap(id, buffer.id);
-    }
-    return *this;
-  }
-  ~Buffer() {
-    release();
-  }
+  Buffer &operator=(Buffer &&buffer) noexcept;
+  ~Buffer();
 
   ALuint id{0};
   Time_point modified; // TODO:
 
 private:
-  Buffer(const mos::aud::Buffer &buffer) {
-    alGenBuffers(1, &id);
-    auto format =
-        buffer.channels() == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16;
-    long data_size = std::distance(buffer.begin(), buffer.end());
-    const ALvoid *data = buffer.data();
-    alBufferData(id, format, data, data_size * sizeof(short),
-                 buffer.sample_rate());
-  }
-  void release() {
-    alDeleteBuffers(1, &id);
-    id = 0;
-  }
+  Buffer(const mos::aud::Buffer &buffer);
+  void release();
 };
 }
